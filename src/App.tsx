cbe4650b504279352278e942a3b7e90ee5a25bc1
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-07-26 15:27:54 -0400

    Set package up to be useable!

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>CTW Component Library</h1>
       <Tester />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
