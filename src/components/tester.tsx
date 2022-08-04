48ee721e8b59bcb9667bcbf0c3778608baca9f8b
Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>
2022-08-04 13:58:33 -0400

    Fix build & cleanup dummy code (#3)
    * Fix issue where we were not building tailwind css in our build step.
    * Remove `Tester` component.
    * Remove dummy `foo` class.

--- src/components/tester.tsx
+++
@@ -1,3 +0,0 @@
-export const Tester = (): JSX.Element => {
-  return <div className="">Hello from tester</div>;
-};
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ export * from "@/components/content/conditions-table";
 export * from "@/components/content/conditions-table-base";
 export * from "@/components/core/ctw-provider";
 export * from "@/components/core/table";
-export * from "@/components/tester";
 export * from "@/styles/tailwind-gen.css";
