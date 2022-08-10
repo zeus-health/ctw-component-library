d97b82feae1f8fb3e6bb53e72a9643c4729295c0
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-08-10 14:12:13 -0400

    Replaced the function of the super long RegEx that determines where to put the prefix, with just typescript. Updated prefixes in repo.

--- scripts/prefixer/README.md
+++ scripts/prefixer/README.md
@@ -4,6 +4,8 @@ Adds the ctw- prefix to every CSS class in any CSS file in the folder where the 
 
 ## Usage
 
+If there is an entire CSS file you don't want the script to pull classes to rename from (e.g. tailwind.css), you can configure this in the script.  
+Then just run the following in the project directory:  
 `npx ts-node --esm scripts/prefixer/prefixer.ts`
 
 ## How
@@ -11,12 +13,7 @@ Adds the ctw- prefix to every CSS class in any CSS file in the folder where the 
 [prefixer.ts](prefixer.ts) generates a mapping of original CSS class names to their prefixed versions based on the CSS files in `/src`.  
 Then, [prefixer_apply](prefixer_apply.ts) applies the mapping to the files in the directory.
 
-[View the Regex in prefixer.ts that adds prefixes to a map](http://regexr.com/6rflc)
-[View the Regex to apply a map on typescript files](http://regexr.com/6rflo)
+## Limitations
 
-## Limitation
-
-This may miss some of the CSS classes (e.g. `.shadow`) or try to rename ones that you don't want to rename (e.g. `#root`). Make sure these edge cases are added/cleaned in [renaming_map.json](renaming_map.json) before you hit apply, or restart but remember to fix the map to undo the mistake.  
-These mistakes will re-appear in the mapping file if you start the script over again, as it is regenerated each time.
-
-If there is an entire CSS file you don't want the script to pull classes to rename from (e.g. tailwind.css), make sure to add it in [prefixer.ts](prefixer.ts) `ignore`.
+This explicitly excludes CSS variables and only prefixes class selectors, not # selectors or keyframes.
+It adds prefixes to class names in CSS files somewhat blindly, so you may need to sometimes manually remove unwanted "ctw-"'s or add in your own new rule with RegEx negative lookahead/lookbehind.
--- scripts/prefixer/prefixer.ts
+++ scripts/prefixer/prefixer.ts
@@ -1,22 +1,27 @@
-import { spawn, spawnSync } from "child_process";
-import {
-  readFile,
-  readFileSync,
-  rmdirSync,
-  writeFile,
-  writeFileSync,
-} from "fs";
+import { spawnSync } from "child_process";
+import { readFile, readFileSync, rmSync, writeFileSync } from "fs";
 import glob from "glob";
 import readline from "readline";
 import rcs from "rename-css-selectors";
+
+// Configurables
+const dir = "./src/**/*";
+const mapDir = "./scripts/prefixer/renaming_map.json";
+const cssIgnore = ["./src/styles/tailwind.css"]; // Filepaths to avoid pulling classes from and prefixing to.
 
 let rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
+let files: string[] = [
+  ...glob.sync(dir + ".ts"),
+  ...glob.sync(dir + ".tsx"),
+  ...glob.sync(dir + ".css", { ignore: cssIgnore }),
+];
+
 tailwindNoPrefixGen();
-draftMap();
+getClasses();
 
 function tailwindNoPrefixGen() {
   let tailwindConfig = readFileSync("tailwind.config.cjs", "utf-8");
@@ -28,10 +33,10 @@ function tailwindNoPrefixGen() {
   });
 }
 
-function draftMap() {
+function getClasses() {
   rcs.process
     .css(
-      glob.sync("./**/*.css", { ignore: ["./src/styles/tailwind.css"] }),
+      glob.sync("./**/*.css", { ignore: cssIgnore }),
       // all css files are now saved, renamed and stored in the selectorLibrary
       {
         ignoreCssVariables: true,
@@ -41,7 +46,7 @@ function draftMap() {
     )
     .then(() => {
       try {
-        rmdirSync("./rcs", { recursive: true });
+        rmSync("./rcs", { recursive: true });
       } catch (err) {
         console.error(`Error while deleting ./rcs.`);
       }
@@ -51,54 +56,83 @@ function draftMap() {
         { overwrite: true },
         (err: any) => {
           // the mapping file draft was generated
-          remap();
+          prefixFiles();
         }
       );
     });
 }
 
-function remap() {
-  // add the prefixes to the mapping file
-  readFile(
-    "./scripts/prefixer/renaming_map.json",
-    "utf-8",
-    (err: any, contents: string) => {
-      const newContent = contents.replace(
-        /(?!"selectors":)(?!": {)(?:"\.?([^a-z]*)([^\s"]+:)?([^a-z]*)(?:ctw-)*([^"]*)":[^,\n]*)/g,
-        '"$1$2$3$4": "$1$2$3ctw-$4"'
-      );
-      writeFile(
-        "./scripts/prefixer/renaming_map.json",
-        newContent,
-        { flag: "w+" },
-        (err) => {
-          rl.question(
-            "Please make sure that the new class names in scripts/renaming_map.json are acceptable and modify accordingly before refactoring. \n [y] - Refactor \n [q] - Quit \n >",
-            (answer) => {
-              switch (answer.toLowerCase()) {
-                case "y":
-                  spawn(
-                    "npx ts-node --esm scripts/prefixer/prefixer_apply.ts",
-                    [],
-                    {
-                      shell: true,
-                      stdio: "inherit",
-                    }
-                  );
-                  break;
-                case "q":
-                  rl.close();
-                  break;
-                default:
-                  console.log("Invalid answer.");
-              }
-              rl.close();
+function prefixFiles() {
+  let map = JSON.parse(readFileSync(mapDir, "utf-8")).selectors;
+
+  for (const file of files) {
+    readFile(file, "utf-8", (err, contents) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      let replaced = contents;
+
+      for (let oldClassName in map) {
+        if (oldClassName.charAt(0) == ".") {
+          // Only select selectors, not for example things that start with #
+          oldClassName = oldClassName.substring(1); // Exclude the dot from counting as part of the class name
+          let toPrefix = oldClassName;
+
+          let maybePseudoClass = "";
+          if (oldClassName.includes(":")) {
+            let splitClassName = oldClassName.split(":");
+            toPrefix = splitClassName.pop() || "";
+            maybePseudoClass = splitClassName.join(":") + ":";
+          }
+
+          let maybeDash = "";
+          if (toPrefix.startsWith("-")) {
+            maybeDash = "-";
+            toPrefix = toPrefix.substring(1);
+          }
+
+          if (!toPrefix.startsWith("ctw-")) {
+            let newClassName = maybePseudoClass + maybeDash + "ctw-" + toPrefix;
+            // Add prefix in CSS file
+            if (file.split(".").pop() === "css") {
+              replaced = replaced.replace(
+                // Replaces non-prefixed appearances of this class name that aren't part of a larger word. Be careful, this can replace things you didn't want to.
+                new RegExp(`(?<!ctw-)(?<!\\w)${oldClassName}(?!\\w)`, "g"),
+                newClassName
+              );
+            } else {
+              // Add prefix in another file (ts/tsx)
+              replaced = replaced.replace(
+                new RegExp(
+                  `(?<! from )` + // Don't replace if it's an import line (with " from " right before it)
+                    `("(?:.*\\s)?)` + // Look for the start of a string, can list classes before this class
+                    oldClassName + // Look for this class inside the string
+                    `((?:\\s.*)?")`, // Look for the end of the string, can list classes before this class
+                  "g"
+                ),
+                "$1" + newClassName + "$2" // Add the classes before and after this one back, along with the renamed class.
+              );
             }
-          );
+          }
         }
-      );
-    }
-  );
+      }
+      writeFileSync(file, replaced, "utf-8");
+    });
+  }
+  rmSync("./scripts/prefixer/renaming_map.json");
+  tailwindPrefixGen();
+}
+
+function tailwindPrefixGen() {
+  let tailwindConfig = readFileSync("tailwind.config.cjs", "utf-8");
+  tailwindConfig = tailwindConfig.replace('prefix: ""', 'prefix: "ctw-"');
+  writeFileSync("tailwind.config.cjs", tailwindConfig, "utf-8");
+  let cssGenerate = spawnSync("npm run generate:css", [], {
+    shell: true,
+    stdio: "inherit",
+  });
+  rl.close();
 }
 
 export {};
