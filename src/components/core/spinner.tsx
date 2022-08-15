08939e0bf079fb4feffafc9efa2c04038acd6670
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-08-15 10:00:15 -0400

    Update spinner.tsx

--- src/components/core/spinner.tsx
+++ src/components/core/spinner.tsx
@@ -9,7 +9,7 @@ export const Spinner = ({ className }: SpinnerProps) => (
     className={cx("ctw-inline ctw-h-4 ctw-w-4 ctw-animate-spin", className)}
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    viewBox="ctw-0 ctw-0 24 24"
+    viewBox="0 0 24 24"
   >
     <circle
       className="ctw-opacity-25"
@@ -22,7 +22,7 @@ export const Spinner = ({ className }: SpinnerProps) => (
     <path
       className="ctw-opacity-75"
       fill="currentColor"
-      d="M4 12a8 8 ctw-0 018-8V0C5.373 ctw-0 ctw-0 5.373 ctw-0 12h4zm2 5.291A7.962 7.962 ctw-0 014 12H0c0 3.042 1.135 5.824 ctw-3 7.938l3-2.647z"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     />
   </svg>
 );
