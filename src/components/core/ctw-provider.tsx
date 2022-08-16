23862ff87b26f3d66dbccc6f74122d2b31f1a0e1
yowon-zus <yyoon@zushealth.com>
2022-08-16 09:03:04 -0400

    WIP: Add auth helpers to handle checking/refresh
    There are a ton of assumptions here:
    1. How to interact with the auth URL to get a new token (I'm not sure the signature makes sense (i.e. depends on how Healthie in this case wants to implement that POST endpoint)
    2. We make `authToken` (legacy) optional, but it adds a bit of complexity in the logic.
    3. Assumption is that expiresInSeconds is actually a date of expiry (in seconds), because we don't know the issue date from the spec

--- src/components/core/ctw-provider.tsx
+++ src/components/core/ctw-provider.tsx
@@ -3,28 +3,80 @@ import * as React from "react";
 
 export type Env = "dev" | "sandbox" | "production";
 
-type CTWState = { env: Env; authToken: string; theme: any };
+const EXPIRY_PADDING_MS = 60000;
+
+type CTWToken = {
+  accessToken: string;
+  issuedTokenType: string;
+  tokenType: string;
+  expiresInSeconds: number;
+}
+
+type CTWState = { 
+  env: Env;
+  authToken?: string;
+  authTokenURL: string;
+  theme: any;
+} & { 
+  token: CTWToken,
+  actions: {
+    handleAuth: () => Promise<null | undefined>;
+  }
+};
 
 type CTWProviderProps = { children: React.ReactNode } & CTWState;
 
 const CTWStateContext = React.createContext<CTWState | undefined>(undefined);
 
 function CTWProvider({ children, ...ctwState }: CTWProviderProps) {
+  const [token, setToken] = React.useState(ctwState.token);
+  const handleAuth = async () => {
+    if (ctwState.authToken) return null;
+    const newToken = await checkOrRefreshAuth(token);
+    if (token.accessToken === newToken.accessToken) return null;
+    setToken(newToken);
+  }
+  const providerState = {
+    ...ctwState,
+    actions: {
+      handleAuth,
+    },
+  }
   return (
-    <CTWStateContext.Provider value={ctwState}>
+    <CTWStateContext.Provider value={providerState}>
       {children}
     </CTWStateContext.Provider>
   );
 }
 
-function useCTW() {
+async function useCTW() {
   const context = React.useContext(CTWStateContext);
   if (context === undefined) {
     throw new Error("useCTW must be used within a CTWProvider");
   }
-
-  const fhirClient = getFhirClient(context.env, context.authToken);
+  await context.actions.handleAuth();
+  const tokenString = context.authToken ?? `${context.token.tokenType} ${context.token.accessToken}`;
+  const fhirClient = getFhirClient(context.env, tokenString);
   return { fhirClient, theme: context.theme };
+}
+
+async function checkOrRefreshAuth(token: CTWToken, url: CTWState["authTokenURL"]): CTWToken {
+  if (Date.now() >= token.expiresInSeconds * 1000 + EXPIRY_PADDING_MS) {
+    try {
+      const data = {
+        duration: 3600, // From Healthie spec.
+      }
+      const response = await fetch(url, {
+        method: "POST",
+        body: data,
+      })
+      const result = await response.json();
+      return result.token;
+    } catch (err) {
+      console.error(err); // TODO: Better error handling.
+    }
+  }
+  return token;
 }
 
 export { CTWProvider, useCTW };
