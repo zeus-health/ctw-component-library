61f9555c75ef667f3a725976ac9485c27478c830
luthfib <luthfibustillos@gmail.com>
2022-08-19 22:13:31 +0200

    rename file

---
+++ src/components/core/table/table-data-cell.tsx
@@ -0,0 +1,38 @@
+import cx from "classnames";
+import { ReactNode } from "react";
+import { MinRecordItem, TableColumn as TableColumnType } from "./table";
+
+type TableColumnProps<T extends MinRecordItem> = {
+  column: TableColumnType<T>;
+  record: T;
+  index: number;
+  showLeftTableBorderShadow: boolean;
+};
+
+export const TableDataCell = <T extends MinRecordItem>({
+  column,
+  record,
+  index,
+  showLeftTableBorderShadow,
+}: TableColumnProps<T>) => {
+  const value = column.dataIndex
+    ? (record[column.dataIndex] as unknown as ReactNode)
+    : undefined;
+
+  return (
+    <td
+      key={column.title ?? index}
+      className={cx(
+        "ctw-whitespace-nowrap ctw-px-3 ctw-py-4 ctw-text-sm",
+        index === 0
+          ? "ctw-font-medium ctw-text-content-black"
+          : "ctw-text-content-light",
+        index === 0 && showLeftTableBorderShadow
+          ? "ctw-table-scroll-left-shadow-sticky ctw-bg-white group-hover:ctw-bg-bg-lighter"
+          : ""
+      )}
+    >
+      {column.render ? column.render(record) : value}
+    </td>
+  );
+};
