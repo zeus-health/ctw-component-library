c1238245f4dc2cbc5dde2b567fde3db9c1e7f89a
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-08-19 11:14:44 -0400

    Setup condition container
    * Add new `Conditions` component which holds our conditions tables and has a link to open a drawer to add new conditions.
    * Also cleaning up some of the non-packaged CSS.

---
+++ src/components/content/condition-form-drawer.tsx
@@ -0,0 +1,35 @@
+import { ConditionModel } from "@/models/conditions";
+import { Drawer } from "../core/drawer";
+
+export type ConditionFormDrawerProps = {
+  editing?: boolean;
+  condition?: ConditionModel;
+  isOpen: boolean;
+  onClose: () => void;
+};
+
+export function ConditionFormDrawer({
+  editing,
+  condition,
+  isOpen,
+  onClose,
+}: ConditionFormDrawerProps) {
+  const title = editing ? "Edit Condition" : "Add Condition";
+
+  return (
+    <Drawer title={title} isOpen={isOpen} onClose={onClose}>
+      <Drawer.Body>TODO</Drawer.Body>
+      <Drawer.Footer>
+        <div className="ctw-flex ctw-justify-end ctw-space-x-3 ctw-text-black ">
+          <button
+            type="button"
+            className="ctw-btn-default ctw-font-semibold ctw-outline-bg-light"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+      </Drawer.Footer>
+    </Drawer>
+  );
+}
