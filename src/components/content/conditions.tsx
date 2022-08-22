44106ad431b5cc2b38e3dd523c1506861e1bf37c
Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>
2022-08-22 16:37:54 -0400

    Misc fixes (#17)
    * Clean up Drawer UI
    * Add column widths (TODO: Set min widths)
    * Fix issue with condition table re-rendering (fixes bug where conditions would reload when opening drawer)
    * Change build target to `es2015`

--- src/components/content/conditions.tsx
+++ src/components/content/conditions.tsx
@@ -5,53 +5,14 @@ import { ConditionsTable } from "./conditions-table";
 
 export type ConditionsProps = {
   patientUPID: string;
-  whichTableToShow?: "confirmed" | "not reviewed" | "all";
   conditionFilter?: ConditionFilters;
 };
 
 export function Conditions({
   patientUPID,
-  whichTableToShow = "all",
   conditionFilter = {},
 }: ConditionsProps) {
   const [addConditionIsOpen, setAddConditionIsOpen] = useState(false);
-
-  const DetermineWhichTableToShow = () => {
-    const ConfirmedTable = (
-      <div className="ctw-space-y-3">
-        <div className="ctw-title ctw-ml-3">Confirmed</div>
-        <ConditionsTable
-          patientUPID={patientUPID}
-          conditionFilter={conditionFilter}
-        />
-      </div>
-    );
-
-    const NotReviewed = (
-      <div className="ctw-space-y-3">
-        <div className="ctw-title ctw-ml-3">Not Reviewed</div>
-        <ConditionsTable
-          patientUPID={patientUPID}
-          isConfirmed={false}
-          showTableHead={false}
-        />
-      </div>
-    );
-
-    switch (whichTableToShow) {
-      case "confirmed":
-        return ConfirmedTable;
-      case "not reviewed":
-        return NotReviewed;
-      default:
-        return (
-          <>
-            {ConfirmedTable}
-            {NotReviewed}
-          </>
-        );
-    }
-  };
 
   return (
     <div className="ctw-border-divider-light ctw-border ctw-border-solid">
@@ -63,7 +24,25 @@ export function Conditions({
       </div>
 
       <div className="ctw-space-y-5 ctw-py-3 ctw-px-4 ">
-        <DetermineWhichTableToShow />
+        <div className="ctw-py-3 ctw-px-4 ctw-space-y-5">
+          <div className="ctw-space-y-3">
+            <div className="ctw-title ctw-ml-3">Confirmed</div>
+            <ConditionsTable
+              patientUPID={patientUPID}
+              isConfirmed={true}
+              conditionFilter={conditionFilter}
+            />
+          </div>
+
+          <div className="ctw-space-y-3">
+            <div className="ctw-title ctw-ml-3">Not Reviewed</div>
+            <ConditionsTable
+              patientUPID={patientUPID}
+              showTableHead={false}
+              isConfirmed={false}
+            />
+          </div>
+        </div>
       </div>
 
       <ConditionFormDrawer
