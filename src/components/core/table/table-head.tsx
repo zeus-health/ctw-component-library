329778f887b292bf9d3e4adb2888cca80d541827
Luthfi Bustillos-Francis <luthfibustillos@gmail.com>
2022-08-22 14:00:54 -0400

    Add lens conditions (#16)
    Adds not reviewed vs reviewed + some stuff for the hackathon/demo (We can cleanup post demo)

--- src/components/core/table/table-head.tsx
+++ src/components/core/table/table-head.tsx
@@ -10,7 +10,7 @@ export const TableHead = <T extends MinRecordItem>({
   columns,
   showLeftTableBorderShadow,
 }: TableHeadProps<T>) => (
-  <thead className="ctw-bg-bg-lighter">
+  <thead>
     <tr>
       {columns.map((column, index) => (
         <th
@@ -19,7 +19,7 @@ export const TableHead = <T extends MinRecordItem>({
           className={cx(
             "ctw-px-3 ctw-py-3 ctw-text-left ctw-text-xs ctw-font-medium ctw-uppercase ctw-tracking-wider ctw-text-content-light",
             index === 0 && showLeftTableBorderShadow
-              ? "ctw-table-scroll-left-shadow-sticky ctw-bg-bg-lighter group-hover:ctw-bg-bg-lighter"
+              ? "ctw-table-scroll-left-shadow-sticky  ctw-bg-white group-hover:ctw-bg-bg-lighter"
               : ""
           )}
         >
