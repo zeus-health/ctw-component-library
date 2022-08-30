9b629432bceda40ffd09e6dc3429fbef5ffa9854
luthfib <luthfibustillos@gmail.com>
2022-08-30 15:52:56 -0400

    reorganize imports

--- src/components/content/conditions.tsx
+++ src/components/content/conditions.tsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from "react";
 
+import { useCTW } from "../core/ctw-provider";
 import { ToggleControl } from "../core/toggle-control";
+
+import { ConditionFormDrawer } from "./condition-form-drawer";
+import { ConditionsTableBase } from "./conditions-table-base";
 
 import {
   ConditionFilters,
@@ -8,9 +12,6 @@ import {
   getLensConditions,
 } from "@/fhir/conditions";
 import { ConditionModel } from "@/models/conditions";
-import { useCTW } from "../core/ctw-provider";
-import { ConditionFormDrawer } from "./condition-form-drawer";
-import { ConditionsTableBase } from "./conditions-table-base";
 
 export type ConditionsProps = {
   patientUPID: string;
@@ -22,16 +23,18 @@ const DEFAULT_ERR_MSG =
 export function Conditions({ patientUPID }: ConditionsProps) {
   const [addConditionIsOpen, setAddConditionIsOpen] = useState(false);
 
-  const [confirmedConditions, setConfirmedConditions] =
-    useState<ConditionModel[]>();
+  const [confirmedConditions, setConfirmedConditions] = useState<
+    ConditionModel[]
+  >([]);
   const [confirmedConditionsMessage, setConfirmedConditionsMessage] = useState(
     "No conditions found"
   );
   const [confirmedConditionsIsLoading, setConfirmedConditionsIsLoading] =
     useState(true);
 
-  const [notReviewedConditions, setNotReviewedConditions] =
-    useState<ConditionModel[]>();
+  const [notReviewedConditions, setNotReviewedConditions] = useState<
+    ConditionModel[]
+  >([]);
   const [notReviewedConditionsIsLoading, setNotConfirmedConditionsIsLoading] =
     useState(true);
   const [notReviewedConditionsMessage, setNotReviewedConditionsMessage] =
@@ -52,6 +55,7 @@ export function Conditions({ patientUPID }: ConditionsProps) {
 
       const fhirClient = await getCTWFhirClient();
 
+      // use AllSettled instead of all as we want confirmed to still if lens fails
       const [confirmedConditionInfo, notReviewedConditionInfo] =
         await Promise.allSettled([
           getConfirmedConditions(fhirClient, patientUPID, conditionFilter),
@@ -61,25 +65,39 @@ export function Conditions({ patientUPID }: ConditionsProps) {
       setNotConfirmedConditionsIsLoading(false);
       setConfirmedConditionsIsLoading(false);
 
+      /* notReviewedConditons depends confirmedConditions so that we can correctly filter out 
+         conditions that appear in confirmedConditions from notReviewedConditons */
       if (confirmedConditionInfo.status === "fulfilled") {
         setConfirmedConditions(
           confirmedConditionInfo.value.map((c) => new ConditionModel(c))
         );
+        const ICD10ConfirmedCodes = confirmedConditionInfo.value.map(
+          (c) => new ConditionModel(c).icd10
+        );
+
+        if (notReviewedConditionInfo.status === "fulfilled") {
+          const notReviewedConditionsFiltered =
+            notReviewedConditionInfo.value.filter(
+              (c) => !ICD10ConfirmedCodes.includes(new ConditionModel(c).icd10)
+            );
+          setNotReviewedConditions(
+            notReviewedConditionsFiltered.map((c) => new ConditionModel(c))
+          );
+        } else {
+          setNotReviewedConditions([]);
+          setNotReviewedConditionsMessage(DEFAULT_ERR_MSG);
+        }
       } else {
         setConfirmedConditions([]);
         setConfirmedConditionsMessage(DEFAULT_ERR_MSG);
-      }
-      if (notReviewedConditionInfo.status === "fulfilled") {
-        setNotReviewedConditions(
-          notReviewedConditionInfo.value.map((c) => new ConditionModel(c))
-        );
-      } else {
         setNotReviewedConditions([]);
         setNotReviewedConditionsMessage(DEFAULT_ERR_MSG);
       }
     }
     load();
-  }, [patientUPID]);
+    // Including getCTWFhirClient causes an infinite loop so disabling this
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [patientUPID, includeInactive]);
 
   return (
     <div className="ctw-border ctw-border-solid ctw-border-divider-light">
@@ -108,9 +126,9 @@ export function Conditions({ patientUPID }: ConditionsProps) {
           <div className="ctw-space-y-3">
             <div className="ctw-title ctw-ml-3">Not Reviewed</div>
             <ConditionsTableBase
-              showTableHead={false}
-              isLoading={notReviewedConditionsIsLoading}
               conditions={notReviewedConditions}
+              isLoading={notReviewedConditionsIsLoading}
+              showTableHead={false}
               message={notReviewedConditionsMessage}
             />
           </div>
