19a1aa10913082e1bc45b4ab1297a7312e3db084
luthfib <luthfibustillos@gmail.com>
2022-09-07 08:05:30 -0500

    fix for keyboard events

--- src/components/core/dropdown-menu.tsx
+++ src/components/core/dropdown-menu.tsx
@@ -1,7 +1,7 @@
 import { Float } from "@headlessui-float/react";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu } from "@headlessui/react";
 import cx from "classnames";
-import { Fragment, ReactNode } from "react";
+import { ReactNode } from "react";
 
 export type MenuItems = {
   name: string;
@@ -16,45 +16,46 @@ export type DropdownMenuProps = {
 
 export function DropdownMenu({ children, menuItems }: DropdownMenuProps) {
   return (
-    <Menu as="div">
-      <Float portal placement="bottom-start" offset={10} flip arrow>
+    <Menu>
+      <Float
+        portal
+        placement="bottom-start"
+        offset={10}
+        flip
+        arrow
+        tailwindcssOriginClass
+      >
         <Menu.Button className="ctw-cursor-pointer ctw-border-none ctw-bg-transparent">
           {children}
         </Menu.Button>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
+
+        <Menu.Items
+          static
+          className="ctw-w-max ctw-bg-white ctw-ring-1 ctw-ring-black ctw-ring-opacity-5"
         >
-          <Menu.Items className="ctw-w-max ctw-bg-white ctw-ring-1 ctw-ring-black ctw-ring-opacity-5">
-            <Float.Arrow className="ctw-absolute ctw-h-5 ctw-w-5 ctw-rotate-45 ctw-border ctw-border-black ctw-bg-white" />
-            <div className="ctw-relative ctw-overflow-hidden ctw-bg-white">
-              {menuItems.map((menuItem) => (
-                <Menu.Item key={menuItem.name}>
-                  {({ active }) => (
-                    <button
-                      onClick={() => menuItem.action()}
-                      type="button"
-                      className={cx(
-                        "ctw-flex ctw-w-full ctw-cursor-pointer ctw-items-center ctw-border-none ctw-bg-transparent ctw-py-2 ctw-px-2 ctw-text-content-black",
-                        {
-                          "hover:ctw-bg-icon-active": active,
-                        },
-                        menuItem.className
-                      )}
-                    >
-                      {menuItem.name}
-                    </button>
-                  )}
-                </Menu.Item>
-              ))}
-            </div>
-          </Menu.Items>
-        </Transition>
+          <Float.Arrow className="ctw-absolute ctw-h-5 ctw-w-5 ctw-rotate-45 ctw-border ctw-border-black ctw-bg-white" />
+          <div className="ctw-relative  ctw-bg-white">
+            {menuItems.map((menuItem) => (
+              <Menu.Item key={menuItem.name}>
+                {({ active }) => (
+                  <button
+                    type="button"
+                    onClick={() => menuItem.action()}
+                    className={cx(
+                      "ctw-flex ctw-w-full ctw-cursor-pointer ctw-items-center ctw-border-none ctw-bg-transparent ctw-py-2 ctw-px-2 ctw-text-content-black",
+                      {
+                        "ctw-bg-icon-active hover:ctw-bg-icon-active": active,
+                      },
+                      menuItem.className
+                    )}
+                  >
+                    {menuItem.name}
+                  </button>
+                )}
+              </Menu.Item>
+            ))}
+          </div>
+        </Menu.Items>
       </Float>
     </Menu>
   );
