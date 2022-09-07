8315bc46b87ef74ded076c7c0c8e6fb3a182f2da
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-09-07 14:44:18 -0400

    Spinner story

--- src/stories/Spinner.stories.tsx
+++ src/stories/Spinner.stories.tsx
@@ -1,11 +1,19 @@
-import { Spinner } from "@/components/core/spinner";
-import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import { ComponentMeta, ComponentStoryFn } from "@storybook/react";
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+import { Spinner } from "@/components/core/spinner";
+
+import "@/styles/tailwind-gen.css";
+
 export default {
   title: "Core/Spinner",
   component: Spinner,
+  argTypes: {
+    className: { name: "className", type: { name: "string", required: false } },
+  },
 } as ComponentMeta<typeof Spinner>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Story: ComponentStoryObj<typeof Spinner> = { args: {} };
+const Template: ComponentStoryFn<typeof Spinner> = (args) => (
+  <Spinner {...args} />
+);
+
+export const Primary = Template.bind({});
