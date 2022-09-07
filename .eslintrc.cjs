f36c5a9f8fc8ce4ca7685023ad5b62c684a0b5ca
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-09-07 12:54:43 -0400

    Reset

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,14 @@
  * @type {import('@types/eslint').Linter.BaseConfig}
  */
 module.exports = {
-  extends: ["@kensho-technologies/eslint-config", "prettier", "plugin:storybook/recommended"],
+  extends: ["@kensho-technologies/eslint-config", "prettier"],
   // Ignore js files as we now have typescript parsing rules.
   // See https://stackoverflow.com/a/65063702 for more.
   ignorePatterns: ["*.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ["./tsconfig.json"]
+    project: ["./tsconfig.json"],
   },
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
@@ -18,17 +18,20 @@ module.exports = {
     "import/extensions": "off",
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
-    "import/no-extraneous-dependencies": ["error", {
-      // The following files can depend on devDependencies.
-      devDependencies: ["*.config.*", "*.env.*"]
-    }]
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        // The following files can depend on devDependencies.
+        devDependencies: ["*.config.*", "*.env.*"],
+      },
+    ],
   },
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but it we have to explicitly
   // set the jest version.
   settings: {
     jest: {
-      version: 27
-    }
-  }
+      version: 27,
+    },
+  },
 };
