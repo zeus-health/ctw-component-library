408995ab8f85f5605d70a955ef7433879489d2f0
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-07 12:41:26 -0400

    Cleanup lint issues & setup CI

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,24 +5,25 @@ module.exports = {
   extends: ["@kensho-technologies/eslint-config", "prettier"],
   // Ignore js files as we now have typescript parsing rules.
   // See https://stackoverflow.com/a/65063702 for more.
-  ignorePatterns: ["*.js"],
+  ignorePatterns: ["*.js", "*.cjs", "vite.config.ts"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    tsconfigRootDir: __dirname,
     project: ["./tsconfig.json"],
   },
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-unnecessary-condition": "error",
+    "@typescript-eslint/no-use-before-define": "off",
     curly: "error",
     "import/extensions": "off",
+    "import/order": "off",
     "import/no-unresolved": "off",
     "import/prefer-default-export": "off",
     "import/no-extraneous-dependencies": [
       "error",
       {
         // The following files can depend on devDependencies.
-        devDependencies: ["*.config.*", "*.env.*"],
+        devDependencies: ["*.config.*", "*.env.*", "*.test.*", "scripts/**/*"],
       },
     ],
   },
