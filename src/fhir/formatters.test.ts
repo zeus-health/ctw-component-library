be24076562cc088611bab927a7aeaa97c5ffd293
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-07 14:56:09 -0400

    Fix test

--- src/fhir/formatters.test.ts
+++ src/fhir/formatters.test.ts
@@ -3,6 +3,6 @@ import { formatPhoneNumber } from "./formatters";
 describe("formatters", () => {
   test("formatPhoneNumber correctly formats phone number", () => {
     const phoneNumber = "1234567890";
-    expect(formatPhoneNumber(phoneNumber)).toEqual("123-456-78901");
+    expect(formatPhoneNumber(phoneNumber)).toEqual("123-456-7890");
   });
 });
