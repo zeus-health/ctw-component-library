f041c64bcb61a5e939f536f02cc90c2887f60e34
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-09-09 14:52:04 -0400

    Added a missing change for the previous commit

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-mock/register",
   ],
   framework: {
     name: "@storybook/react-vite",
