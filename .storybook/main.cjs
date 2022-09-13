56d313c2ef363fe1d25b3362030cc88398db3291
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-09-13 11:11:38 -0400

    Remove failed data fetch mocking attempt

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,7 +4,6 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "storybook-addon-mock/register",
   ],
   framework: {
     name: "@storybook/react-vite",
