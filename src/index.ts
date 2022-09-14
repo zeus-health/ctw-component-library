d1f77d652f5b8a9842faa7ec7ca55fccbcb021f1
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-14 13:45:18 -0400

    Add comment

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "@/components/content/conditions-table-base";
 export * from "@/components/core/ctw-provider";
 export * from "@/components/core/patient-provider";
 export * from "@/components/core/table/table";
+// This is what allows vite to build dist/style.css
 export * from "@/styles/tailwind-gen.css";
