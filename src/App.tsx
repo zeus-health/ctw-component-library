eb076f0017c7551fba3089fb388548802d9d1d7b
luthfib <luthfibustillos@gmail.com>
2022-09-15 09:21:39 -0500

    use env vars instead

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import { CTWProvider } from "./components/core/ctw-provider";
 import { PatientProvider } from "./components/core/patient-provider";
 import "./styles/tailwind-gen.css";
 
-const AUTH_TOKEN = import.meta.env.VITE_AUTH_TOKEN;
-const SB_SARAH_ID = "000002";
-const SYSTEM_HEALTHIE_ID = "https://www.ctwhealth.com";
+const { SYSTEM_URL, AUTH_TOKEN, PATIENT_ID } = import.meta.env;
 
 function App() {
   return (
     <CTWProvider env="dev" authToken={AUTH_TOKEN} theme={{}}>
-      <PatientProvider patientID={SB_SARAH_ID} systemURL={SYSTEM_HEALTHIE_ID}>
+      <PatientProvider patientID={PATIENT_ID} systemURL={SYSTEM_URL}>
         <div className="App ctw-space-y-5">
           <h1>CTW Component Library</h1>
           <Conditions />
