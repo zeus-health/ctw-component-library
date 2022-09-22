b63f024c9f490431edc4bc4ede293e647c1f3b0a
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-22 13:44:46 -0400

    Simplify table and refactor styles to table.scss

--- src/components/core/table/table.tsx
+++ src/components/core/table/table.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 
 import { TableHead } from "./table-head";
 import { TableRows } from "./table-rows";
+import "./table.scss";
 
 export interface MinRecordItem {
   id: string | number;
@@ -22,7 +23,6 @@ export type TableProps<T extends MinRecordItem> = {
   className?: string;
   records: T[];
   columns: TableColumn<T>[];
-  onRowClick?: (row: T) => void; // Adds a row hover effect and calls onClick.
   isLoading?: boolean;
   message?: string;
   showTableHead?: boolean;
@@ -37,32 +37,22 @@ export const Table = <T extends MinRecordItem>({
   className,
   columns,
   records,
-  onRowClick,
   isLoading = false,
   message = "No records found",
   showTableHead = true,
 }: TableProps<T>) => (
-  <div className={cx("ctw-py-2 ctw-align-middle", className)}>
-    <div
-      /* Border radius has to be same as table. 
-           For some reason the overflow: hidden also hides the table borders, so add some padding to get them back. */
-      className="ctw-relative ctw-overflow-hidden ctw-rounded-lg ctw-p-px"
-    >
-      <div className="ctw-flex ctw-overflow-x-auto ctw-rounded-lg ctw-shadow ctw-ring-1 ctw-ring-divider-light ctw-ring-opacity-5">
-        <table className="ctw-table-base ctw-w-full ctw-divide-y ctw-divide-divider-main">
-          {showTableHead && <TableHead columns={columns} />}
+  <div className={cx("ctw-table-container", className)}>
+    <table>
+      {showTableHead && <TableHead columns={columns} />}
 
-          <tbody className="ctw-divide-y ctw-divide-divider-light ctw-bg-white">
-            <TableRows
-              records={records}
-              columns={columns}
-              onRowClick={onRowClick}
-              isLoading={isLoading}
-              emptyMessage={message}
-            />
-          </tbody>
-        </table>
-      </div>
-    </div>
+      <tbody>
+        <TableRows
+          records={records}
+          columns={columns}
+          isLoading={isLoading}
+          emptyMessage={message}
+        />
+      </tbody>
+    </table>
   </div>
 );
