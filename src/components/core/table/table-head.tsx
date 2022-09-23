38cfc7ec27cf9c487c4fc4fba2aabdfeb7e8e8f3
Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>
2022-09-23 11:42:12 -0400

    Refactor styles (#72)
    Switching to use dynamic importing with SCSS (sass). This allows each of
    our components to specify and import their own CSS. Refactored a handful of CSS to do this.
    
    Additionally removed `onRowClick` from table as it is unused.

--- src/components/core/table/table-head.tsx
+++ src/components/core/table/table-head.tsx
@@ -1,5 +1,3 @@
-import cx from "classnames";
-
 import type { MinRecordItem, TableColumn } from "./table";
 
 export type TableHeadProps<T extends MinRecordItem> = {
@@ -15,10 +13,7 @@ export const TableHead = <T extends MinRecordItem>({
         <th
           key={column.title ?? index}
           scope="col"
-          className={cx(
-            "ctw-px-3 ctw-py-3 ctw-text-left ctw-text-xs ctw-font-medium ctw-uppercase ctw-tracking-wider ctw-text-content-light",
-            column.className
-          )}
+          className={column.className}
         >
           {column.title}
         </th>
