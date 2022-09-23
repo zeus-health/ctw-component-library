973fd8d26f85d325bec6233eec83d8a882b743a5
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-23 15:18:29 -0400

    Add back table scroll shadows

--- src/components/core/table/table.tsx
+++ src/components/core/table/table.tsx
@@ -1,5 +1,5 @@
 import cx from "classnames";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 
 import { TableHead } from "./table-head";
 import { TableRows } from "./table-rows";
@@ -43,37 +43,74 @@ export const Table = <T extends MinRecordItem>({
   message = "No records found",
   showTableHead = true,
 }: TableProps<T>) => {
+  const tableRef = useRef<HTMLTableElement>(null);
+  const containerRef = useRef<HTMLTableElement>(null);
+  const [showLeftShadow, setShowLeftShadow] = useState(false);
+  const [showRightShadow, setShowRightShadow] = useState(false);
+  const updateShadows = () => {
+    const container = containerRef.current;
+    const table = tableRef.current;
+    if (container && table) {
+      setShowLeftShadow(container.scrollLeft > 0);
+      const rightSide = container.scrollLeft + container.clientWidth;
+      setShowRightShadow(rightSide < table.clientWidth);
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+    updateShadows();
+    container?.addEventListener("scroll", updateShadows);
+    window.addEventListener("resize", updateShadows);
+
+    return () => {
+      container?.removeEventListener("scroll", updateShadows);
+      window.removeEventListener("resize", updateShadows);
+    };
+  }, [containerRef]);
+
   const hasData = !isLoading && records.length > 0;
 
   return (
-    <div className={cx("ctw-table-container", className)}>
-      <table>
-        {hasData && (
-          <colgroup>
-            {columns.map((column, index) => (
-              <col
-                key={column.title ?? index}
-                className={column.className}
-                style={{
-                  minWidth: column.minWidth,
-                  width: `${column.widthPercent}%`,
-                }}
-              />
-            ))}
-          </colgroup>
-        )}
+    <div
+      className={cx(
+        "ctw-table-container",
+        {
+          "ctw-table-scroll-left-shadow": showLeftShadow,
+          "ctw-table-scroll-right-shadow": showRightShadow,
+        },
+        className
+      )}
+    >
+      <div className="ctw-scrollbar" ref={containerRef}>
+        <table ref={tableRef}>
+          {hasData && (
+            <colgroup>
+              {columns.map((column, index) => (
+                <col
+                  key={column.title ?? index}
+                  className={column.className}
+                  style={{
+                    minWidth: column.minWidth,
+                    width: `${column.widthPercent}%`,
+                  }}
+                />
+              ))}
+            </colgroup>
+          )}
 
-        {showTableHead && hasData && <TableHead columns={columns} />}
+          {showTableHead && hasData && <TableHead columns={columns} />}
 
-        <tbody>
-          <TableRows
-            records={records}
-            columns={columns}
-            isLoading={isLoading}
-            emptyMessage={message}
-          />
-        </tbody>
-      </table>
+          <tbody>
+            <TableRows
+              records={records}
+              columns={columns}
+              isLoading={isLoading}
+              emptyMessage={message}
+            />
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
