241e8241eb9c9f2abc2156316405690b93f56277
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-09-27 14:40:49 -0400

    Fix ESLint error
    Obviously our wrapper hook needs to import useLayoutEffect

--- src/hooks/use-isomorphic-layout-effect.ts
+++ src/hooks/use-isomorphic-layout-effect.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
 import { useEffect, useLayoutEffect } from "react";
 
 // Wraps use of useLayoutEffect in a guard to protect against SSR uses.
