1447f5dd08c096094bf0dd6c2aaaa0dfd9ea3023
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-10-07 15:43:02 -0400

    Refactor some code out of main conditions component

---
+++ src/components/content/conditions-no-patient.tsx
@@ -0,0 +1,23 @@
+import cx from "classnames";
+import { AlertDialog } from "../core/alert";
+
+export function ConditionsNoPatient({ className }: { className?: string }) {
+  return (
+    <div className={cx("ctw-conditions", className)}>
+      <div className="ctw-conditions-heading-container">
+        <div className="ctw-title">Conditions</div>
+      </div>
+      <div className="ctw-p-5">
+        <AlertDialog header="Conditions Unavailable">
+          <div>
+            We are unable to access Condition information for this patient.
+          </div>
+          <div>
+            Contact your system administrator or customer service for
+            assistance.
+          </div>
+        </AlertDialog>
+      </div>
+    </div>
+  );
+}
