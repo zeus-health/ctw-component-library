7ce3c373cc1ca59481fa23fa64984fee4be06c53
luthfib <luthfibustillos@gmail.com>
2022-10-07 13:40:05 -0400

    fix error catching

--- src/fhir/practitioner.ts
+++ src/fhir/practitioner.ts
@@ -1,6 +1,13 @@
 import Client from "fhir-kit-client";
 import { searchBuilderRecords } from "./search-helpers";
 
+class PractitionerNotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "PractitionerNotFound";
+  }
+}
+
 export const getPractitioner = async (
   practitionerId: string,
   fhirClient: Client
@@ -15,13 +22,21 @@ export const getPractitioner = async (
     );
 
     if (practitioners.length === 0) {
-      throw new Error(`No practitioner found with an id of: ${practitionerId}`);
+      throw new PractitionerNotFoundError(
+        `No practitioner found with an id of: ${practitionerId}`
+      );
     }
 
     return practitioners[0];
-  } catch (e) {
+  } catch (error) {
+    if (error instanceof PractitionerNotFoundError) {
+      throw new PractitionerNotFoundError(
+        `No practitioner found with an id of: ${practitionerId}`
+      );
+    }
+
     throw new Error(
-      `Failed fetching practitioner with an id of: ${practitionerId}. ${e}`
+      `Failed fetching practitioner with an id of: ${practitionerId}. ${error}`
     );
   }
 };
