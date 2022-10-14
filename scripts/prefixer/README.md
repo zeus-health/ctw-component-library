9eaba31d9a03e7ebbdb1ab43ed231da721bc404c
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-10-14 15:48:18 -0400

    Temporary prefixer script fix

--- scripts/prefixer/README.md
+++ scripts/prefixer/README.md
@@ -1,18 +1,17 @@
 # Prefixer
 
-Adds the "ctw-" (or other) prefix to CSS classes in any CSS/TS/TSX file, so long as it does not already have a prefix.
+Blindly adds the "ctw-" (or other) prefix to CSS selectors to a specified file, to help initially move components from CTW App to this library.
 
 ## Usage
 
-1. If needed, manually add CSS files to ignore to cssIgnore constant in prefixer.ts. These files will be skipped and not used for prefixing.
+1. Set the configurables at the top of `scripts/prefixer/prefixer.ts`
 1. Run the following in the project directory: `npx ts-node --esm scripts/prefixer/prefixer.ts`
-
-## How
-
-A package generates a mapping of original CSS class names based on the CSS files in `/src`. Then the script reads these and replaces them with prefixed versions in each file.
+1. Look over the new file and make manual corrections.
 
 ## Limitations
 
+Assume there are extra or missing prefixes, or that prefixes may be added in a way that doesn't match how tailwind prefixes it. This replaces very loosely.
+
+- It assumes every word in a string is a selector to be prefixed.
+- It uses RegEx to find what and where to prefix in the files
 - This doesn't _change_ prefixes, only add them.
-- This explicitly excludes CSS variables and only prefixes class selectors, not # selectors or keyframes.
-- It uses RegEx to find where to prefix in the files, there may be extra or missing prefixes due to cases not accounted for.
--- scripts/prefixer/prefixer.ts
+++ scripts/prefixer/prefixer.ts
@@ -1,138 +1,24 @@
 /* eslint-disable no-console */
-import { spawnSync } from "child_process";
-import { existsSync, mkdirSync, readFileSync, rmSync, writeFileSync } from "fs";
-import glob from "glob";
-import rcs from "rename-css-selectors";
+import { readFileSync, writeFileSync } from "fs";
 
 // Configurables
-const dir = "./src/**/*";
-const mapFile = "./scripts/prefixer/tmp/renaming_map.json";
-// - Filepaths to avoid pulling classes from and prefixing to.
-const cssIgnore = [
-  "./src/styles/tailwind.css",
-  "./src/styles/tailwind-gen.css", // Ignore this file because the script generates the list of unprefixed classes in /tmp
-];
+const target = "./src/components/core/modal.tsx";
 const prefix = "ctw-";
 
 async function main() {
-  tailwindNoPrefixGen();
-  await getClasses();
-  addPrefixes(filesToPrefix());
-  rmSync("scripts/prefixer/tmp", { recursive: true }); // Clear the temp directory
-}
-
-function tailwindNoPrefixGen() {
-  const tailwindConfig = readFileSync("tailwind.config.cjs", "utf-8");
-  if (!existsSync("./scripts/prefixer/tmp")) {
-    mkdirSync("./scripts/prefixer/tmp");
-  }
-  writeFileSync(
-    "scripts/prefixer/tmp/tailwind.config.cjs",
-    tailwindConfig.replace(/prefix: "\S*",\n/g, "")
-  );
-  const cssGenerate = spawnSync(
-    "npx tailwindcss -c ./scripts/prefixer/tmp/tailwind.config.cjs -i ./src/styles/tailwind.css -o ./scripts/prefixer/tmp/tailwind-gen-unprefixed.css",
-    [],
-    {
-      shell: true,
-      stdio: "inherit",
-    }
-  );
-}
-
-// Produce the json file with the CSS classes
-async function getClasses() {
-  await rcs.process.css(
-    glob.sync("./**/*.css", { ignore: cssIgnore }),
-    // all css files are now saved, renamed and stored in the selectorLibrary
-    {
-      ignoreCssVariables: true,
-      preventRandomName: true,
-      replaceKeyframes: true,
-    }
-  );
-  rmSync("./rcs", { recursive: true });
-  await rcs.mapping.generate("./scripts/prefixer/tmp", { overwrite: true });
-}
-
-// Get the files the prefixes need to be added to
-function filesToPrefix(): string[] {
-  return [
-    ...glob.sync(`${dir}.ts`),
-    ...glob.sync(`${dir}.tsx`),
-    ...glob.sync(`${dir}.css`, { ignore: cssIgnore }),
-  ];
-}
-
-// Add classname prefixes to a series of files
-function addPrefixes(files: string[]) {
-  const stringsToPrefix = getStringsToPrefix();
-
-  files.forEach((file) => {
-    console.log(`Processing ${file}`);
-    // Add prefix in CSS file
-    let replaced = readFileSync(file, "utf-8");
-    stringsToPrefix.forEach((toPrefixUnconverted) => {
-      const toPrefix = toPrefixUnconverted.toString();
-      replaced = replaced.replace(
-        // Replaces non-prefixed appearances of this class name that aren't part of a larger word. Be careful, this can replace things you didn't want to.
-        getReplaceRegEx(file, toPrefix),
-        `ctw-${toPrefix}`
-      );
-      console.log(`\tReplaced "${toPrefix}" with "ctw-${toPrefix}"`);
-    });
-    writeFileSync(file, replaced, "utf-8");
-  });
-}
-
-function getReplaceRegEx(file: string, toPrefix: string): RegExp {
-  if (file.split(".").pop() === "css") {
-    // Add prefix in a CSS file
-    return new RegExp(
-      `(?<!\\w|\\w-)` + // Not part of another name (part 1)
-        `(?<!${prefix})${
-          // Not already prefixed
-          toPrefix
-        }(?!\\w|-|:)`, // Not part of another name (part 2), nor a CSS property
+  const replaced = readFileSync(target, "utf-8").replaceAll(
+    // Replaces non-prefixed appearances of this class name that aren't part of a larger word. Be careful, this can replace things you didn't want to.
+    new RegExp(
+      `(?<!import .*)` + // Not an import
+        `(?<!\\w|\\w-|\\.)` + // Not part of another name, incl not already prefixed (part 1), not a property/function
+        `(?<!//.*)` + // Not part of a comment
+        `(?<=".*)` + // Preceded somewhere on that line by a quotation mark
+        `([\\w-:]+)`, // Thing to prefix
       "g"
-    );
-  }
-  // Add prefix in another file (ts/tsx)
-  return new RegExp(
-    `(?<!import .*)` + // Not an import
-      `(?<!\\w|\\w-|\\.)` + // Not part of another name, incl not already prefixed (part 1), not a property/function
-      `(?<!//.*)` + // Not part of a comment
-      `(?<!${prefix})${
-        // Not already prefixed
-        toPrefix
-      }(?!\\w|-|\\.)`, // Not part of another name (part 2), not an object
-    "g"
+    ),
+    `${prefix}$1`
   );
-}
-
-// Get just the list of the parts of the string that we need to prefix
-function getStringsToPrefix(): Set<string> {
-  const map = JSON.parse(readFileSync(mapFile, "utf-8")).selectors;
-  const stringsToPrefix = new Set<string>();
-  map.forEach((oldClassName: string) => {
-    if (oldClassName.charAt(0) === ".") {
-      // Only select selectors starting with ., not for example things that start with #
-      let toPrefix = oldClassName.substring(1); // Exclude the dot from counting as part of the class name
-
-      // Take out any pseudo classes
-      toPrefix = oldClassName.includes(":")
-        ? oldClassName.split(":").pop() || ""
-        : toPrefix;
-      // Take out the dash prefix if there is one
-      toPrefix = toPrefix.startsWith("-") ? toPrefix.substring(1) : toPrefix;
-
-      if (!toPrefix.startsWith(prefix)) {
-        stringsToPrefix.add(toPrefix);
-        console.log(`Planning to prefix the string "${toPrefix}"`);
-      }
-    }
-  });
-  return stringsToPrefix;
+  writeFileSync(target, replaced, "utf-8");
 }
 
 main();
