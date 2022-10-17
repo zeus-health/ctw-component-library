8ca8564608b94a8219b6cbf7159716ba7baa3678
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-10-17 12:43:29 -0400

    Hardcode prefixer

--- scripts/prefixer/README.md
+++ scripts/prefixer/README.md
@@ -1,17 +1,13 @@
 # Prefixer
 
-Blindly adds the "ctw-" (or other) prefix to CSS selectors to a specified file, to help initially move components from CTW App to this library.
+Adds the "ctw-" (or other) prefix to unprefixed Tailwind CSS selectors in a target file. Helps initially move components from CTW App to this library.
 
 ## Usage
 
-1. Set the configurables at the top of `scripts/prefixer/prefixer.ts`
+1. Set the target file in `scripts/prefixer/prefixer.ts`
 1. Run the following in the project directory: `npx ts-node --esm scripts/prefixer/prefixer.ts`
-1. Look over the new file and make manual corrections.
 
 ## Limitations
 
-Assume there are extra or missing prefixes, or that prefixes may be added in a way that doesn't match how tailwind prefixes it. This replaces very loosely.
-
-- It assumes every word in a string is a selector to be prefixed.
-- It uses RegEx to find what and where to prefix in the files
-- This doesn't _change_ prefixes, only add them.
+Uses regex to find what to replace and where, so may not perfectly replace each instance of the class name.
+If an incorrect or missing renaming was found, edit the `renamings.json` file.
--- scripts/prefixer/prefixer.ts
+++ scripts/prefixer/prefixer.ts
@@ -1,23 +1,26 @@
 /* eslint-disable no-console */
 import { readFileSync, writeFileSync } from "fs";
+import * as renamings from "scripts/prefixer/renamings.json";
 
 // Configurables
 const target = "./src/components/core/modal.tsx";
-const prefix = "ctw-";
 
 async function main() {
-  const replaced = readFileSync(target, "utf-8").replaceAll(
-    // Replaces non-prefixed appearances of this class name that aren't part of a larger word. Be careful, this can replace things you didn't want to.
-    new RegExp(
-      `(?<!import .*)` + // Not an import
-        `(?<!\\w|\\w-|\\.)` + // Not part of another name, incl not already prefixed (part 1), not a property/function
-        `(?<!//.*)` + // Not part of a comment
-        `(?<=".*)` + // Preceded somewhere on that line by a quotation mark
-        `([\\w-:]+)`, // Thing to prefix
-      "g"
-    ),
-    `${prefix}$1`
-  );
+  let replaced = readFileSync(target, "utf-8");
+  Object.entries(renamings).forEach(([selector, replacement]) => {
+    replaced = replaced.replaceAll(
+      // Replaces non-prefixed appearances of this class name that aren't part of a larger word. Be careful, this can replace things you didn't want to.
+      new RegExp(
+        `(?<!import .*)` + // Not an import
+          `(?<!\\w|\\w-|\\.)` + // Not part of another name, incl not already prefixed (part 1), not a property/function
+          `(?<!//.*)` + // Not part of a comment
+          `(?<=".*)` + // Preceded somewhere on that line by a quotation mark
+          `${selector}`, // Thing to prefix
+        "g"
+      ),
+      replacement
+    );
+  });
   writeFileSync(target, replaced, "utf-8");
 }
 
