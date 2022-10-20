516289ddaa1864f9a3e78811e4911e7c0dd8e62d
Ada Cieńciała <86961468+ada-cienciala@users.noreply.github.com>
2022-10-20 11:18:45 +0200

    [CTW-406] Add pagination component (#129)
    Ticket: [CTW-406](https://zeushealth.atlassian.net/browse/CTW-406)
    Figma:
    [pagination](https://www.figma.com/file/PMFiTmKkExhuPjpInXjrp3/Implementation-(Healthie)?node-id=2164%3A2950)
    
    There are 4 use cases to be tested:
    1. For <=10 Conditions - no pagination
    2. For >10 Conditions - Show All button
    3. For >20 Conditions - Show More button (next 10 conditions shown) and
    Show All button
    3. For all >10 conditions shown - Reset button (first 10 conditions
    shown)
    
    1st, 2nd, and 4th use cases (`patientId:
    dad67efb-749f-47d7-9600-7f24f33dcc94`):
    
    
    https://user-images.githubusercontent.com/86961468/196726084-7fe5c587-c7f6-4497-87a1-ab4841e3baba.mov
    
    3rd  use case (`patientId: 8ba07acb-0dac-450e-911d-3609d9b51a4b`):
    
    
    https://user-images.githubusercontent.com/86961468/196725960-d12a807d-b7bb-4cdc-a971-8dce4b62defa.mov

---
+++ src/components/core/pagination/pagination.tsx
@@ -0,0 +1,56 @@
+export const DEFAULT_PAGE_SIZE = 10;
+
+export type PaginationProps = {
+  currentPage: number;
+  pageSize: number;
+  total: number;
+  onNext: () => void;
+  onAll: () => void;
+  onReset: () => void;
+};
+
+export const Pagination = ({
+  currentPage,
+  pageSize = DEFAULT_PAGE_SIZE,
+  total,
+  onNext,
+  onAll,
+  onReset,
+}: PaginationProps) => {
+  const currentNumber = Math.min(currentPage * pageSize, total);
+  const allShown = currentNumber === total;
+
+  return (
+    <div className="ctw-flex ctw-items-center ctw-justify-between ctw-py-3 ctw-px-6">
+      <div className="ctw-text-gray-600 ctw-text-sm">
+        Showing <span className="ctw-font-medium">{currentNumber}</span> of{" "}
+        <span className="ctw-font-medium">{total}</span> results
+      </div>
+      <div className="ctw-flex ctw-h-full ctw-justify-end ctw-space-x-3">
+        {!allShown && total > 20 && (
+          <button type="button" className="ctw-btn-default" onClick={onNext}>
+            Show More
+          </button>
+        )}
+        {!allShown && (
+          <button
+            type="button"
+            className="ctw-btn-primary ctw-w-28 ctw-whitespace-nowrap"
+            onClick={onAll}
+          >
+            Show All
+          </button>
+        )}
+        {allShown && total > 10 && (
+          <button
+            type="button"
+            className="ctw-btn-primary ctw-w-28 ctw-whitespace-nowrap"
+            onClick={onReset}
+          >
+            Reset
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
