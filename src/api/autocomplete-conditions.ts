3bdc8799389a6b3207f92a3c50a2316a99db593c
luthfib <luthfibustillos@gmail.com>
2022-10-21 12:09:38 -0400

    remove unused function

--- src/api/autocomplete-conditions.ts
+++ src/api/autocomplete-conditions.ts
@@ -1,25 +1,8 @@
-import { QueryFunctionContext } from "@tanstack/react-query";
 import React from "react";
 import { Env } from "..";
 import { getFormsConditionsUrl } from "./urls";
 
 export type QueryKeyAutoCompleteConditions = [string, string, Env, string];
-
-export const getAutoCompleteConditions = async (
-  queryParams: QueryFunctionContext<QueryKeyAutoCompleteConditions>
-) => {
-  const { queryKey } = queryParams;
-  const [_, authToken, env, searchTerm] = queryKey;
-
-  const response = await fetch(
-    `${getFormsConditionsUrl(env)}?display=${searchTerm}`,
-    {
-      headers: { Authorization: `Bearer ${authToken}` },
-    }
-  );
-  const data = await response.json();
-  return data.conditionsList;
-};
 
 export const setAutoCompleteConditions = async (
   authToken: string,
