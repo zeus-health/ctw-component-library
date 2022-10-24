afb2b48b331498b9555da8e0f0a9e41eb84e5051
Adam Zambrzycki <68335773+azambrzycki-htd@users.noreply.github.com>
2022-10-24 13:46:06 +0200

    [CTW-401] Add way to check library version for component library (#140)
    [CTW-401](https://zeushealth.atlassian.net/browse/CTW-401)
    
    - Added library version to window global variable
    - Exported library version so customer can use it in their own debug
    view
    
    <img width="330" alt="Screenshot 2022-10-20 at 13 13 21"
    src="https://user-images.githubusercontent.com/68335773/196934355-c684053a-daf9-4792-8528-4d451d50cb66.png">

---
+++ .changeset/thin-tomatoes-pump.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": minor
+---
+
+Add library version to window.CTWComponentLibrary.version variable which allows version to export to dependents.
--- src/components/core/ctw-provider.tsx
+++ src/components/core/ctw-provider.tsx
@@ -8,6 +8,7 @@ import {
   ReactNode,
   useCallback,
   useContext,
+  useEffect,
   useMemo,
   useRef,
   useState,
@@ -19,6 +20,7 @@ import {
   CTWToken,
 } from "./ctw-context";
 import "./main.scss";
+import { version } from "../../../package.json";
 
 export type Env = "dev" | "sandbox" | "production";
 
@@ -35,9 +37,23 @@ type CTWProviderProps = {
   headers?: HeadersInit;
 } & (AuthTokenSpecified | AuthTokenURLSpecified);
 
+declare global {
+  interface Window {
+    CTWComponentLibrary: {
+      version: string;
+    };
+  }
+}
+
 function CTWProvider({ theme, children, ...ctwState }: CTWProviderProps) {
   const [token, setToken] = useState<CTWToken>();
   const ctwProviderRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    window.CTWComponentLibrary = {
+      version,
+    };
+  }, []);
 
   const handleAuth = useCallback(async () => {
     if (ctwState.authToken) {
