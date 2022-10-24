a059fc7e017f2d6c88ae7a5d1b713134251d42fc
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-10-24 14:06:29 -0400

    Remove extra copy of a function

--- src/api/autocomplete-conditions.ts
+++ src/api/autocomplete-conditions.ts
@@ -1,38 +1,5 @@
-import {
-  SYSTEM_CONDITION_CLINICAL,
-  SYSTEM_CONDITION_VERIFICATION_STATUS,
-} from "@/fhir/system-urls";
-import { Condition } from "fhir/r4";
 import { Env } from "..";
 import { getFormsConditionsUrl } from "./urls";
-
-// Sets any autofill values that apply when a user adds a condition, whether creating or confirming.
-export function setAddConditionDefaults(condition: Condition): void {
-  const addDefaults: Partial<Condition> = {
-    clinicalStatus: {
-      coding: [
-        {
-          system: SYSTEM_CONDITION_CLINICAL,
-          code: "active",
-          display: "Active",
-        },
-      ],
-      text: "active",
-    },
-    verificationStatus: {
-      coding: [
-        {
-          system: SYSTEM_CONDITION_VERIFICATION_STATUS,
-          code: "confirmed",
-          display: "Confirmed",
-        },
-      ],
-      text: "confirmed",
-    },
-  };
-
-  Object.assign(condition, addDefaults);
-}
 
 export const getAutoCompleteConditions = async (
   authToken: string,
