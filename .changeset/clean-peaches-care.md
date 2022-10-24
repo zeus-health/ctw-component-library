5fe44bcbdec1d52a6811196d7f9df680e3c6669c
luthfib <luthfibustillos@gmail.com>
2022-10-24 15:20:27 -0400

    add changeset

---
+++ .changeset/clean-peaches-care.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": minor
+---
+
+Adds way to display code best on order of preference by system, favoring enrichment. Fixes bug around error message in condiiton not being descriptive.
--- src/components/core/form/combobox-field.tsx
+++ src/components/core/form/combobox-field.tsx
@@ -63,12 +63,7 @@ export const ComboboxField = <T,>({
         placeholder="Type to search"
       />
 
-      <input
-        hidden
-        name={name}
-        value={inputValueParsed as string}
-        onChange={(e) => console.log("HEEEEY", e)}
-      />
+      <input hidden name={name} value={inputValueParsed as string} />
       <Combobox.Options className="ctw-listbox ctw-max-h-60 ctw-overflow-auto ctw-rounded-md ctw-bg-white ctw-py-1 ctw-text-base ctw-shadow-lg ctw-ring-1 ctw-ring-black ctw-ring-opacity-5 focus:ctw-outline-none sm:ctw-text-sm">
         <ComboboxOptions options={options} query={searchTerm} />
       </Combobox.Options>
