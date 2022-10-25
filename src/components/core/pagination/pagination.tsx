3a06d5ffb6a92be30a6b583a420876f438bf393b
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-10-25 18:34:10 -0400

    Fix bug "showing 10 of 0 results" while loading

--- src/components/core/pagination/pagination.tsx
+++ src/components/core/pagination/pagination.tsx
@@ -12,7 +12,8 @@ export const Pagination = ({ total, count, changeCount }: PaginationProps) => {
   return (
     <div className="ctw-pagination ctw-flex ctw-items-center ctw-justify-between ctw-py-1 ctw-px-6">
       <div className="ctw-text-gray-600 ctw-text-sm">
-        Showing <span className="ctw-font-medium">{count}</span> of{" "}
+        Showing{" "}
+        <span className="ctw-font-medium">{Math.min(count, total)}</span> of{" "}
         <span className="ctw-font-medium">{total}</span> results
       </div>
       <div className="ctw-flex ctw-h-full ctw-justify-end ctw-space-x-3">
