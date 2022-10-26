f5b214c55b1a170662b0dcc8c63d04482984c32f
ada-cienciala <adrianna.cienciala@htdevelopers.com>
2022-10-26 11:50:34 +0200

    Dont show buttons in pagination when no records

--- src/components/core/pagination/pagination.tsx
+++ src/components/core/pagination/pagination.tsx
@@ -7,20 +7,14 @@ export type PaginationProps = {
 };
 
 export const Pagination = ({ total, count, changeCount }: PaginationProps) => {
-  const allShown = count === total;
+  const allShown = count === total || total === 0;
 
   return (
     <div className="ctw-pagination ctw-flex ctw-items-center ctw-justify-between ctw-px-6">
       <div className="ctw-text-gray-600 ctw-text-sm">
-        {total === 0 ? (
-          <>No results found</>
-        ) : (
-          <>
-            Showing{" "}
-            <span className="ctw-font-medium">{Math.min(count, total)}</span> of{" "}
-            <span className="ctw-font-medium">{total}</span> results
-          </>
-        )}
+        Showing{" "}
+        <span className="ctw-font-medium">{Math.min(count, total)}</span> of{" "}
+        <span className="ctw-font-medium">{total}</span> results
       </div>
       <div className="ctw-flex ctw-h-full ctw-justify-end ctw-space-x-3">
         {!allShown && total > DEFAULT_PAGE_SIZE * 2 && (
