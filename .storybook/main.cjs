d7186c90f5d570321667558d7690b00723ac1e3b
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-10-26 15:32:32 -0400

    Prettier + Disable Telemetry

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,16 +1,15 @@
-const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-}
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
