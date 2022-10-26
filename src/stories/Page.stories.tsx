25bc867a7bf91da6e424f87fa04a2663c5c09d51
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-10-26 15:25:49 -0400

    Get example stories working
    1. Removed importing of 'react' as that is handled automatically.
    2. Exported interfaces as that was causing issues in stories.
    3. Switched away from deprecated ComponentMeta in favor of Meta
    4. Removed introduction mdx file as I couldn't get it working.

--- src/stories/Page.stories.tsx
+++ src/stories/Page.stories.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import type { ComponentStoryFn, ComponentMeta } from '@storybook/react';
-import { within, userEvent } from '@storybook/testing-library';
-import { Page } from './Page';
+import type { ComponentStoryFn, Meta } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+import { Page } from "./Page";
 
 export default {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
   },
-} as ComponentMeta<typeof Page>;
+} as Meta<typeof Page>;
 
 const Template: ComponentStoryFn<typeof Page> = (args) => <Page {...args} />;
 
@@ -21,6 +20,6 @@ export const LoggedIn = Template.bind({});
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 LoggedIn.play = async ({ canvasElement }) => {
   const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+  const loginButton = await canvas.getByRole("button", { name: /Log in/i });
   await userEvent.click(loginButton);
 };
