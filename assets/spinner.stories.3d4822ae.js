65d1160d6f8683e470022e91839bc824db54110e
mrosata <michael.rosata@gmail.com>
2022-11-15 17:00:55 +0000

    Deploying to gh-pages from  @ 19547c190e084248fef05e118615de3acc826054 🚀

--- assets/spinner.stories.3d4822ae.js
+++
@@ -1,2 +0,0 @@
-import{S as o}from"./spinner.27123330.js";import"./index.51a8318a.js";import"./iframe.a900e634.js";import"./index.d3b8680b.js";const e={component:o,tags:["docsPage"]},i={},p=["Basic"];export{i as Basic,p as __namedExportsOrder,e as default};
-//# sourceMappingURL=spinner.stories.3d4822ae.js.map
