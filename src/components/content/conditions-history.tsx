1e95eee2d24d908f35b5d822861f130fe449bca1
Melody Liu <33408946+melocule@users.noreply.github.com>
2022-11-16 13:53:04 -0500

    CTW-352 Dedupe Conditions History (#251)
    CTW-352
    Prevents duplicate history records from appearing, based on uniqueness
    of all displayed fields.
    
    --- Example ---
    Before, entries 3 and 4 are duplicates. Entry 1 differs slightly, entry
    5 differs significantly.
    <img width="191" alt="image"
    src="https://user-images.githubusercontent.com/33408946/202228612-5e70e799-86eb-4b49-a9f2-3b63ee5d3237.png">
    
    After, only one of 3 and 4 is kept and other records remain separate.
    <img width="200" alt="image"
    src="https://user-images.githubusercontent.com/33408946/202228258-8f137560-0f45-463d-8f4b-23d0664cdc8f.png">

--- src/components/content/conditions-history.tsx
+++ src/components/content/conditions-history.tsx
@@ -2,7 +2,7 @@ import { Loading } from "@/components/core/loading";
 import { getIncludedResources } from "@/fhir/bundle";
 import { useConditionHistory } from "@/fhir/conditions";
 import { ConditionModel } from "@/fhir/models/condition";
-import { capitalize, orderBy, startCase } from "lodash";
+import { capitalize, isEqual, orderBy, startCase, uniqWith } from "lodash";
 import { useEffect, useState } from "react";
 import { CodingList } from "../core/coding-list";
 import { CollapsibleDataListProps } from "../core/collapsible-data-list";
@@ -102,18 +102,14 @@ export function ConditionHistory({ condition }: { condition: ConditionModel }) {
           (c) => c.verificationStatus !== "entered-in-error"
         );
 
-        const conditionsFilteredWithDate =
-          filterEnteredinErrorConditions.filter((c) => c.recordedDate);
-        const conditionsFilteredWithoutDate =
-          filterEnteredinErrorConditions.filter((c) => !c.recordedDate);
-
-        setConditionsWithDate(
-          conditionsFilteredWithDate.map((model) => setupData(model))
+        const conditionsDataDeduped = uniqWith(
+          filterEnteredinErrorConditions.map((model) => setupData(model)),
+          (a, b) => isEqual(a.data, b.data)
         );
 
-        setConditionsWithoutDate(
-          conditionsFilteredWithoutDate.map((model) => setupData(model))
-        );
+        setConditionsWithDate(conditionsDataDeduped.filter((d) => d.date));
+        setConditionsWithoutDate(conditionsDataDeduped.filter((d) => !d.date));
+
         setLoading(false);
       }
     }
