6b443878486cb10e57e0360493ca2c48943a0a0a
Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>
2022-11-16 11:46:31 -0500

    CTW-523/storybook conditions (#250)
    Add condition stories and fix a few bugs:
    1. Don't show multiple `required` condition errors when adding a
    condition.
    2. Hide empty table's Add Condition button when readOnly
    3. Don't modify the actual other provider condition when adding and
    setting default

---
+++ .changeset/short-zebras-press.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": patch
+---
+
+Fix two minor condition bugs.
---
+++ src/components/content/conditions/mocks/forms-data-conditions-search.ts
@@ -0,0 +1,66 @@
+export const heartConditions = {
+  total: 12,
+  conditionsList: [
+    {
+      display: "Asymptomatic coronary heart disease (disorder)",
+      code: "315348000",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Chronic ischemic heart disease (disorder)",
+      code: "413838009",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Conduction disorder of the heart (disorder)",
+      code: "44808001",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Congenital heart disease (disorder)",
+      code: "13213009",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Congestive heart failure (disorder)",
+      code: "42343007",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Heart block (disorder)",
+      code: "233916004",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Heart failure (disorder)",
+      code: "84114007",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Heart valve disorder (disorder)",
+      code: "368009",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Hypertensive heart disease (disorder)",
+      code: "64715009",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display:
+        "Hypertensive heart disease with congestive heart failure (disorder)",
+      code: "5148006",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Left heart failure (disorder)",
+      code: "85232009",
+      system: "http://snomed.info/sct",
+    },
+    {
+      display: "Rheumatic heart disease (disorder)",
+      code: "23685000",
+      system: "http://snomed.info/sct",
+    },
+  ],
+};
