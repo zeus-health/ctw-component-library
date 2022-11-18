07eb326cda3adda9f9c7c5370258d04f9141f1dc
luthfib <luthfibustillos@gmail.com>
2022-11-18 21:42:42 +0000

    Deploying to gh-pages from  @ 4e9e3fe0ee481ab4e9ac419bf5ffe92dc125758e 🚀

--- assets/data-list.stories.bb8a917d.js
+++
@@ -1,2 +0,0 @@
-import{D as a}from"./data-list.9d34f1a8.js";import"./index.50bef666.js";import"./iframe.fbc10f67.js";const o={component:a,tags:["docsPage"]},s={args:{title:"data list title",data:[{label:"String value",value:"hello world"},{label:"Array value",value:["One","two","3"]},{label:"Undefined value",value:void 0}]}},i=["Basic"];export{s as Basic,i as __namedExportsOrder,o as default};
-//# sourceMappingURL=data-list.stories.bb8a917d.js.map
