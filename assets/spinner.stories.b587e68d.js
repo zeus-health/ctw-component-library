e09a135ac27d430143655912f74260eb559ad761
melocule <33408946+melocule@users.noreply.github.com>
2022-11-23 15:29:19 +0000

    Deploying to gh-pages from  @ 839f22c3db3a1a6632582f02447980aebf1bc344 🚀

---
+++ assets/spinner.stories.b587e68d.js
@@ -0,0 +1,2 @@
+import{S as o}from"./spinner.0515b392.js";import"./index.768dbde9.js";import"./iframe.0dba12bf.js";import"./index.d3b8680b.js";const e={component:o,tags:["docsPage"]},i={},p=["Basic"];export{i as Basic,p as __namedExportsOrder,e as default};
+//# sourceMappingURL=spinner.stories.b587e68d.js.map
