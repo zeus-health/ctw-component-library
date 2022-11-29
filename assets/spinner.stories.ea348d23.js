05f99fa4ebcfbceee18cea84f3c6d7c86038e340
luthfib <luthfibustillos@gmail.com>
2022-11-29 21:58:38 +0000

    Deploying to gh-pages from  @ bd7df29fef636a627d3adba12dda425d66afe120 🚀

---
+++ assets/spinner.stories.ea348d23.js
@@ -0,0 +1,2 @@
+import{S as o}from"./spinner.4a0d3f4f.js";import"./index.9b4eecc8.js";import"./iframe.1eaa3164.js";import"./index.d3b8680b.js";const e={component:o,tags:["docsPage"]},i={},p=["Basic"];export{i as Basic,p as __namedExportsOrder,e as default};
+//# sourceMappingURL=spinner.stories.ea348d23.js.map
--- assets/spinner.stories.ffdf7339.js
+++
@@ -1,2 +0,0 @@
-import{S as o}from"./spinner.b10ea4d0.js";import"./index.4b2e442c.js";import"./iframe.ae86db6c.js";import"./index.d3b8680b.js";const e={component:o,tags:["docsPage"]},i={},p=["Basic"];export{i as Basic,p as __namedExportsOrder,e as default};
-//# sourceMappingURL=spinner.stories.ffdf7339.js.map
