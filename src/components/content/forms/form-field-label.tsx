d70c33e25447300d1eb209ebb3c010a79c2a5c5c
luthfib <luthfibustillos@gmail.com>
2022-12-01 14:55:00 -0500

    add optional keyword

--- src/components/content/forms/form-field-label.tsx
+++ src/components/content/forms/form-field-label.tsx
@@ -9,6 +9,11 @@ export const FormFieldLabel = ({
 }) => (
   <div className="ctw-flex ctw-justify-between">
     <label htmlFor={name}>{label}</label>
+    {!required && (
+      <span className="ctw-right-0 ctw-inline-block ctw-text-xs ctw-text-content-light">
+        Optional
+      </span>
+    )}
     {required && (
       <>
         <div className="ctw-flex-grow ctw-text-icon-default">*</div>
