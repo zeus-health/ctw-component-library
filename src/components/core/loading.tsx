b0ddd94faa30e50518343a278304876882ee462a
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-12-05 15:30:48 -0500

    Minor fix to loading spinner

--- src/components/core/loading.tsx
+++ src/components/core/loading.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/core/spinner";
 
 export const Loading = ({ message = "Loading..." }) => (
   <div className="ctw-space-x-2">
-    <span className="ctw-text-sm ctw-italic">{message}</span>
+    {message && <span className="ctw-text-sm ctw-italic">{message}</span>}
     <Spinner />
   </div>
 );
