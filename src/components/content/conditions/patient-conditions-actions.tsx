a4f008e01fa32c1268efd1c76f5724238bb533de
Wesley Reitzfeld <wesley@kapowcorp.com>
2022-12-08 14:49:07 -0500

    Setup condition add

--- src/components/content/conditions/patient-conditions-actions.tsx
+++ src/components/content/conditions/patient-conditions-actions.tsx
@@ -1,5 +1,16 @@
 import { PlusIcon } from "@heroicons/react/outline";
+import { curry } from "lodash";
+import { useState } from "react";
+import {
+  conditionAddSchema,
+  getAddConditionData,
+} from "../forms/condition-schema";
+import { createOrEditCondition } from "../forms/conditions";
+import { DrawerFormWithFields } from "@/components/core/form/drawer-form-with-fields";
+import { usePatient } from "@/components/core/patient-provider";
 import { Toggle } from "@/components/core/toggle";
+import { getNewCondition } from "@/fhir/conditions";
+import { ConditionModel } from "@/fhir/models";
 
 export type PatientConditionsActionsProps = {
   onToggleShowHistoric: () => void;
@@ -8,6 +19,16 @@ export type PatientConditionsActionsProps = {
 export function PatientConditionsActions({
   onToggleShowHistoric,
 }: PatientConditionsActionsProps) {
+  const patientResponse = usePatient();
+  const patientId = patientResponse.data?.id ?? "";
+  const [isAddDrawerOpen, setIsAddDrawerOpen] = useState(false);
+
+  const newCondition = new ConditionModel(getNewCondition(patientId));
+  const newConditionData = getAddConditionData({
+    condition: newCondition,
+  });
+  const handleAddAction = curry(createOrEditCondition)(newCondition, patientId);
+
   return (
     <div className="ctw-flex ctw-items-center ctw-justify-end ctw-space-x-2 ctw-border-0 ctw-border-t ctw-border-solid ctw-border-divider-light ctw-py-5 ctw-px-4">
       <Toggle
@@ -15,9 +36,22 @@ export function PatientConditionsActions({
         text="Show Historic"
         onChange={onToggleShowHistoric}
       />
-      <button type="button" className="ctw-btn-icon">
+      <button
+        type="button"
+        className="ctw-btn-icon"
+        onClick={() => setIsAddDrawerOpen(true)}
+      >
         <PlusIcon className="ctw-h-4 ctw-w-4" />
       </button>
+
+      <DrawerFormWithFields
+        title="Add Condition"
+        action={handleAddAction}
+        data={newConditionData}
+        schema={conditionAddSchema}
+        isOpen={isAddDrawerOpen}
+        onClose={() => setIsAddDrawerOpen(false)}
+      />
     </div>
   );
 }
