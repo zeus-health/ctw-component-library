2493071882963e6c5dde9173b74de2db8cf27639
luthfib <luthfibustillos@gmail.com>
2022-12-23 03:58:28 +0000

    Deploying to gh-pages from  @ 2f071256fe4e72ceb331c647dad394c6864d4cd7 🚀

---
+++ assets/patient-medications.stories.2a6dc7a3.js
@@ -0,0 +1,7 @@
+import{R as e}from"./index.9f56b196.js";import{s as a,p as s,o as n}from"./requests.1438fc36.js";import{w as p,a as c,P as m,S as d}from"./index.591021d6.js";import"./_commonjsHelpers.712cc82f.js";import"./table.6dce43b2.js";import"./index.d3b8680b.js";import"./spinner.51ecf2bb.js";import"./index.7dcc3bb2.js";import"./extends.bed14d96.js";import"./drawer.0ebe46ba.js";import"./index.f2b75861.js";import"./data-list.6d7c3503.js";import"./action-list.8d2557fc.js";const k={tags:["docsPage"],component:p,decorators:[(r,{args:i})=>e.createElement(c,{env:"dev",authToken:"ey.12345",builderId:"12345"},e.createElement(m,{patientID:"007",systemURL:d},e.createElement(r,{args:i})))]},t={...a({providerMedications:s,otherProviderMedications:n})};var o;t.parameters={...t.parameters,storySource:{source:`{
+  ...setupMedicationMocks({
+    providerMedications,
+    otherProviderMedications
+  })
+}`,...(o=t.parameters)==null?void 0:o.storySource}};const T=["Basic"];export{t as Basic,T as __namedExportsOrder,k as default};
+//# sourceMappingURL=patient-medications.stories.2a6dc7a3.js.map
