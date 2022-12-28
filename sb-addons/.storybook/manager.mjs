90e399670b8316494602fd7cb65f711e93af74f9
WesleyKapow <1568246+WesleyKapow@users.noreply.github.com>
2022-12-28 16:33:05 +0000

    Deploying to gh-pages from  @ f378f2fd7601809a36e9452aa9b065f09fa9671f 🚀

--- sb-addons/.storybook/manager.mjs
+++
@@ -1,8 +0,0 @@
-try{
-var n=`
-*[id*="test"] {
-  display: none !important;
-}
-`;if(location.hostname==="zeus-health.github.io"){let t=document.head||document.getElementsByTagName("head")[0],e=document.createElement("style");t.appendChild(e),e.appendChild(document.createTextNode(n))}
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=manager.mjs.map
