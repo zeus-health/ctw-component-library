7c463dc18c22675491c5e2857a60f091b3aef2b3
WesleyKapow <1568246+WesleyKapow@users.noreply.github.com>
2022-12-29 15:30:52 +0000

    Deploying to gh-pages from  @ a792a470c861d38ff7a1908854bebe72b6571e68 🚀

---
+++ assets/action-list.stories.4c79b672.js
@@ -0,0 +1,17 @@
+import{A as d}from"./action-list.149bd7dd.js";import"./index.9f56b196.js";import"./_commonjsHelpers.712cc82f.js";import"./index.d3b8680b.js";const M={component:d,tags:["docsPage"]},e=(c="",m="",p="",u=!1)=>({id:c,title:m,subtitle:p,complete:u}),i=[e("007","Miralax Oral Product","Dissolve 17g in 4\u20138oz liquid and drink once daily for up to 7 days."),e("123","3 ML insulin glargine 100 UNT/ML Pen Injector [Lantus]","Inject 3 ML with enclosed pen injector every morning."),e("insulin","3 ML insulin aspart protamine, human 70 UNT/ML / insulin aspart, human 30 UNT/ML Pen Injector [NovoLog Mix] ","Inject 3 ML before meals. Quantity: 90 days"),e("next","triamcinolone acetonide 0.147 MG/ML Topical Spray","Apply to affected area as needed for eczema exacerbation.",!0)],t={args:{items:i}},o={args:{items:i,onUndoAction:void 0}},n={args:{items:[]}};var a;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    items
+  }
+}`,...(a=t.parameters)==null?void 0:a.storySource}};var r;o.parameters={...o.parameters,storySource:{source:`{
+  args: {
+    items,
+    // Storybook will automatically add action callbacks so we have
+    // to explictily set it to undefined.
+    onUndoAction: undefined
+  }
+}`,...(r=o.parameters)==null?void 0:r.storySource}};var s;n.parameters={...n.parameters,storySource:{source:`{
+  args: {
+    items: []
+  }
+}`,...(s=n.parameters)==null?void 0:s.storySource}};const f=["Basic","WithoutUndo","Empty"];export{t as Basic,n as Empty,o as WithoutUndo,f as __namedExportsOrder,M as default};
+//# sourceMappingURL=action-list.stories.4c79b672.js.map
--- assets/patient-medications.stories.76ecd852.js
+++
@@ -1,7 +0,0 @@
-import{R as e}from"./index.9f56b196.js";import{s as a,p as s,o as n}from"./requests.d8bf71ae.js";import{y as p,a as c,P as m,S as d}from"./index.cfba3697.js";import"./_commonjsHelpers.712cc82f.js";import"./action-list.8d2557fc.js";import"./index.d3b8680b.js";import"./table.e8870acf.js";import"./spinner.51ecf2bb.js";import"./index.7dcc3bb2.js";import"./extends.bed14d96.js";import"./drawer.0ebe46ba.js";import"./index.f2b75861.js";import"./data-list.6d7c3503.js";const k={tags:["docsPage"],component:p,decorators:[(r,{args:i})=>e.createElement(c,{env:"dev",authToken:"ey.12345",builderId:"12345"},e.createElement(m,{patientID:"007",systemURL:d},e.createElement(r,{args:i})))]},t={...a({providerMedications:s,otherProviderMedications:n})};var o;t.parameters={...t.parameters,storySource:{source:`{
-  ...setupMedicationMocks({
-    providerMedications,
-    otherProviderMedications
-  })
-}`,...(o=t.parameters)==null?void 0:o.storySource}};const T=["Basic"];export{t as Basic,T as __namedExportsOrder,k as default};
-//# sourceMappingURL=patient-medications.stories.76ecd852.js.map
