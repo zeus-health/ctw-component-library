f4ff40c4a909ef17e6586e3266e896d1fabb7faf
WesleyKapow <1568246+WesleyKapow@users.noreply.github.com>
2023-01-03 19:24:52 +0000

    Deploying to gh-pages from  @ 395501b0aa79df35d4beb15fc72801afbe331019 🚀

---
+++ assets/action-list.stories-011ada7b.js
@@ -0,0 +1,17 @@
+import{A as u}from"./action-list-31f88619.js";import"./index-6f814c40.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-74f03c09.js";const M={component:u,tags:["docsPage"]},e=(c="",m="",p="",d=!1)=>({id:c,title:m,subtitle:p,complete:d}),i=[e("007","Miralax Oral Product","Dissolve 17g in 4–8oz liquid and drink once daily for up to 7 days."),e("123","3 ML insulin glargine 100 UNT/ML Pen Injector [Lantus]","Inject 3 ML with enclosed pen injector every morning."),e("insulin","3 ML insulin aspart protamine, human 70 UNT/ML / insulin aspart, human 30 UNT/ML Pen Injector [NovoLog Mix] ","Inject 3 ML before meals. Quantity: 90 days"),e("next","triamcinolone acetonide 0.147 MG/ML Topical Spray","Apply to affected area as needed for eczema exacerbation.",!0)],t={args:{items:i}},o={args:{items:i,onUndoAction:void 0}},n={args:{items:[]}};var a;t.parameters={...t.parameters,storySource:{source:`{
+  args: {
+    items
+  }
+}`,...(a=t.parameters)==null?void 0:a.storySource}};var r;o.parameters={...o.parameters,storySource:{source:`{
+  args: {
+    items,
+    // Storybook will automatically add action callbacks so we have
+    // to explictily set it to undefined.
+    onUndoAction: undefined
+  }
+}`,...(r=o.parameters)==null?void 0:r.storySource}};var s;n.parameters={...n.parameters,storySource:{source:`{
+  args: {
+    items: []
+  }
+}`,...(s=n.parameters)==null?void 0:s.storySource}};const f=["Basic","WithoutUndo","Empty"];export{t as Basic,n as Empty,o as WithoutUndo,f as __namedExportsOrder,M as default};
+//# sourceMappingURL=action-list.stories-011ada7b.js.map
