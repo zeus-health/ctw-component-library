3bf7f39c7467075fdddc30e19bf83882f6f5cb89
Priyank Madria <98342697+pmadria3@users.noreply.github.com>
2023-01-09 13:20:56 -0600

    Ctw 626/CCDA-follow-up (#370)
    [CTW-626](https://zeushealth.atlassian.net/jira/software/projects/CTW/boards/49?selectedIssue=CTW-626)
    
    This PR is a cleanup and addition of a few things: 
    
    ICON: 
    - Updated to 2.0 icon and added the icon in the header of for the
    collapsible card
    ![Screenshot 2023-01-05 at 2 03 24
    PM](https://user-images.githubusercontent.com/98342697/210869875-c6de462b-bebf-418f-b203-fe09efd24ceb.png)
    
    - Download functionality and cleanup of the code 
    ![Screenshot 2023-01-05 at 2 04 02
    PM](https://user-images.githubusercontent.com/98342697/210869988-7934c490-5c59-421d-9f47-762aa498739f.png)
    
    
    
    [CTW-626]:
    https://zeushealth.atlassian.net/browse/CTW-626?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/components/content/conditions/story-helpers/mocks/requests.ts
+++ src/components/content/conditions/story-helpers/mocks/requests.ts
@@ -2,6 +2,7 @@ import { cloneDeep } from "lodash";
 import { rest } from "msw";
 import { ComponentType, createElement } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { conditionBinary } from "./condition-binary";
 import { heartConditions } from "./forms-data-conditions-search";
 import { historyChronsDisease } from "./history-crohns-disease";
 import { historyDermatitis } from "./history-dermatitis";
@@ -43,6 +44,7 @@ export function setupConditionMocks({
         mockConditionPost,
         mockConditionPut,
         mockPatientHistoryGet,
+        mockConditionHistoryBinaryDocument,
         mockConditionsBasic,
         mockConditionVersionHistoryBundle,
         mockConditionProvenance,
@@ -68,22 +70,32 @@ const mockPatientGet = rest.get(
   "https://api.dev.zusapi.com/fhir/Patient",
   // Add ctx.delay(750), delay to show loading, we set this to 750ms to be
   // less than the default testing-library timeout of 1000ms.
-  (req, res, ctx) => res(ctx.delay(750), ctx.status(200), ctx.json(patient))
+  (_, res, ctx) => res(ctx.delay(750), ctx.status(200), ctx.json(patient))
 );
 
 const mockPatientHistoryGet = rest.get(
   "https://api.dev.zusapi.com/patient-history/messages",
-  (req, res, ctx) => res(ctx.status(200), ctx.json(patientHistoryMessage))
+  (_, res, ctx) => res(ctx.status(200), ctx.json(patientHistoryMessage))
 );
 
 const mockProvenancePost = rest.post(
   "https://api.dev.zusapi.com/fhir/Provenance",
-  (req, res, ctx) => res(ctx.status(200))
+  (_, res, ctx) => res(ctx.status(200))
+);
+
+const mockConditionHistoryBinaryDocument = rest.get(
+  "https://api.dev.zusapi.com/fhir/Binary",
+  async (_, res, ctx) => res(ctx.status(200), ctx.json(conditionBinary))
+);
+
+const mockConditionProvenance = rest.get(
+  "https://api.dev.zusapi.com/fhir/Provenance?target=Condition/:Condition",
+  async (_, res, ctx) => res(ctx.status(200), ctx.json(ProvenanceCondition))
 );
 
 const mockConditionSearch = rest.get(
   "https://api.dev.zusapi.com/forms-data/terminology/conditions",
-  (req, res, ctx) => res(ctx.status(200), ctx.json(heartConditions))
+  (_, res, ctx) => res(ctx.status(200), ctx.json(heartConditions))
 );
 
 // Mocked post will add the new condition to the
@@ -160,11 +172,6 @@ const mockConditionsBasic = rest.post(
     otherConditionsCache.total = otherConditionsCache.entry?.length;
     return res(ctx.status(200), ctx.json(newBasicResource));
   }
-);
-
-const mockConditionProvenance = rest.get(
-  "https://api.dev.zusapi.com/fhir/Provenance?target=Condition/:Condition",
-  async (req, res, ctx) => res(ctx.status(200), ctx.json(ProvenanceCondition))
 );
 
 // To handle batch request for conditions used to get version history.
