be32d6a970ba83f922bfa8571fb8a75bbc05932d
Michael Rosata <mrosata@zushealth.com>
2023-01-23 12:39:11 -0500

    Added storybook tests for <PatientMedications /> (#383)
    This PR added 3 patient medications tests and added `data-testid` to some form elements to make targeting them in tests easier.

---
+++ src/components/content/medications/story-helpers/medication-form-drawer.ts
@@ -0,0 +1,22 @@
+import { userEvent, within } from "@storybook/testing-library";
+
+export function medicationFormDrawer(canvasElement: HTMLElement) {
+  const canvas = within(canvasElement);
+  const drawer = within(canvas.getByRole("dialog"));
+
+  return {
+    search: (search: string) =>
+      userEvent.type(drawer.getByPlaceholderText("Type to search"), search),
+    selectMedication: async (name: string) =>
+      userEvent.click(await drawer.findByRole("option", { name })),
+    status: (status: string) =>
+      userEvent.selectOptions(drawer.getByTestId("form-field-status"), status),
+    instructions: (instructions: string) =>
+      userEvent.type(drawer.getByLabelText("Instructions"), instructions),
+    cancel: () =>
+      userEvent.click(drawer.getByRole("button", { name: "Cancel" })),
+    save: async () => {
+      userEvent.click(drawer.getByRole("button", { name: "Save" }));
+    },
+  };
+}
