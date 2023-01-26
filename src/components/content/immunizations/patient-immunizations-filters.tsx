164f76be7936d91ea629d06279a957c13f67cd92
Priyank <pmadria@zushealth.com>
2023-01-26 15:06:54 -0500

    dedupe immunizations

---
+++ src/components/content/immunizations/patient-immunizations-filters.tsx
@@ -0,0 +1,19 @@
+import { ImmunizationModel } from "@/fhir/models/immunization";
+import { isEqual, uniqWith } from "@/utils/nodash";
+
+export const applyImmunizationFilters = (data: fhir4.Immunization[]) => {
+  const immunizationModel = data.map(
+    (immunization) => new ImmunizationModel(immunization)
+  );
+  const immunizationData = uniqWith(immunizationModel, (a, b) =>
+    isEqual(valuesToDedupeOn(a), valuesToDedupeOn(b))
+  );
+
+  return immunizationData;
+};
+
+const valuesToDedupeOn = (immunization: ImmunizationModel) => [
+  immunization.description,
+  immunization.cvxCode,
+  immunization.occurance,
+];
--- src/fhir/immunizations.ts
+++ src/fhir/immunizations.ts
@@ -1,6 +1,5 @@
-import { getIncludedBasics } from "./bundle";
-import { ImmunizationModel } from "./models/immunization";
 import { searchCommonRecords } from "./search-helpers";
+import { applyImmunizationFilters } from "@/components/content/immunizations/patient-immunizations-filters";
 import { useQueryWithPatient } from "@/components/core/providers/patient-provider";
 import { orderBy } from "@/utils/nodash";
 import { QUERY_KEY_PATIENT_IMMUNIZATIONS } from "@/utils/query-keys";
@@ -18,23 +17,16 @@ export function usePatientImmunizations() {
             patientUPID: patient.UPID,
           }
         );
-        const models = setupImmunizationModels(immunizations, bundle);
-        return orderBy(models, [(model) => model.occurance ?? ""], ["desc"]);
+        return orderBy(
+          applyImmunizationFilters(immunizations),
+          [(model) => model.occurance ?? ""],
+          ["desc"]
+        );
       } catch (e) {
         throw new Error(
           `Failed fetching immunization information for patient: ${e}`
         );
       }
     }
-  );
-}
-
-function setupImmunizationModels(
-  resources: fhir4.Immunization[],
-  bundle: fhir4.Bundle
-): ImmunizationModel[] {
-  const basicsMap = getIncludedBasics(bundle);
-  return resources.map(
-    (c) => new ImmunizationModel(c, undefined, basicsMap.get(c.id ?? ""))
   );
 }
