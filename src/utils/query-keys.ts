ecf3e710210debcc32d88ae91152766b38b44b9f
Michael Rosata <mrosata@zushealth.com>
2023-01-26 11:16:10 -0500

    Offsite/allergies (#424)
    Added `<PatientAllergies />` POC component.
    
    ![Screen Shot 2023-01-26 at 11 04 50
    AM](https://user-images.githubusercontent.com/6380075/214886283-b2165a37-7c0f-4823-93f3-ba9b6fe977c2.png)

--- src/utils/query-keys.ts
+++ src/utils/query-keys.ts
@@ -1,3 +1,4 @@
+export const QUERY_KEY_PATIENT_ALLERGIES = "patientAllergies";
 export const QUERY_KEY_PATIENT_CONDITIONS = "patientConditions";
 export const QUERY_KEY_OTHER_PROVIDER_CONDITIONS = "otherProviderConditions";
 export const QUERY_KEY_CONDITION_HISTORY = "conditionHistory";
