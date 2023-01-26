800aa63397a7a6f4f0eb58ae557099cd0ee8e263
Priyank <pmadria@zushealth.com>
2023-01-26 15:23:20 -0500

    fix

--- src/components/content/allergies/allergies-filter.tsx
+++ src/components/content/allergies/allergies-filter.tsx
@@ -1,9 +1,11 @@
 import { AllergyModel } from "@/fhir/models/allergies";
 import { pick, uniqBy } from "@/utils/nodash/fp";
 
-export const applyAllergyFilters = (data: fhir4.AllergyIntolerance[]) => {
-  return uniqBy(pickDedupeValues, data.map((allergy) => new AllergyModel(allergy));
-};
+export const applyAllergyFilters = (data: fhir4.AllergyIntolerance[]) =>
+  uniqBy(
+    pickDedupeValues,
+    data.map((allergy) => new AllergyModel(allergy))
+  );
 
 const pickDedupeValues = pick([
   "categories",
