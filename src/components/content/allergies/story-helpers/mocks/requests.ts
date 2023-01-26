6980644141cfeac2adb0275ccf95913ea1b88b66
Michael Rosata <mrosata@zushealth.com>
2023-01-26 11:08:40 -0500

    cleanup

--- src/components/content/allergies/story-helpers/mocks/requests.ts
+++ src/components/content/allergies/story-helpers/mocks/requests.ts
@@ -23,8 +23,7 @@ export function setupAllergiesMocks({
 
 const mockPatientGet = rest.get(
   "https://api.dev.zusapi.com/fhir/Patient",
-  // Add ctx.delay(750), delay to show loading, we set this to 750ms to be
-  // less than the default testing-library timeout of 1000ms.
+  // Add ctx.delay(750), delay to show loading, we set this to 750ms
   (req, res, ctx) => res(ctx.delay(750), ctx.status(200), ctx.json(patient))
 );
 
