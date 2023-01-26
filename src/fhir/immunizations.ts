448c21f7455f45ef6d329e3714965e444206e7bb
Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>
2023-01-26 12:11:01 -0500

    Add PatientImmunization component (#429)

---
+++ src/fhir/immunizations.ts
@@ -0,0 +1,40 @@
+import { getIncludedBasics } from "./bundle";
+import { ImmunizationModel } from "./models/immunization";
+import { searchCommonRecords } from "./search-helpers";
+import { useQueryWithPatient } from "@/components/core/providers/patient-provider";
+import { orderBy } from "@/utils/nodash";
+import { QUERY_KEY_PATIENT_ENCOUNTERS } from "@/utils/query-keys";
+
+export function usePatientImmunizations() {
+  return useQueryWithPatient(
+    QUERY_KEY_PATIENT_ENCOUNTERS,
+    [],
+    async (requestContext, patient) => {
+      try {
+        const { bundle, resources: immunizations } = await searchCommonRecords(
+          "Immunization",
+          requestContext,
+          {
+            patientUPID: patient.UPID,
+          }
+        );
+        const models = setupImmunizationModels(immunizations, bundle);
+        return orderBy(models, [(model) => model.occurance ?? ""], ["desc"]);
+      } catch (e) {
+        throw new Error(
+          `Failed fetching immunization information for patient: ${e}`
+        );
+      }
+    }
+  );
+}
+
+function setupImmunizationModels(
+  resources: fhir4.Immunization[],
+  bundle: fhir4.Bundle
+): ImmunizationModel[] {
+  const basicsMap = getIncludedBasics(bundle);
+  return resources.map(
+    (c) => new ImmunizationModel(c, undefined, basicsMap.get(c.id ?? ""))
+  );
+}
