696ef648272dd5cf01abbe77616d6c5208241f67
Michael Rosata <mrosata@zushealth.com>
2023-01-27 13:10:31 -0500

    allergies heading and better card view, fhir button icon

--- src/components/content/allergies/patient-allergies-column.tsx
+++ src/components/content/allergies/patient-allergies-column.tsx
@@ -1,35 +1,39 @@
 import { TableColumn } from "@/components/core/table/table-helpers";
 import { ViewFHIR } from "@/components/core/view-fhir";
 import { AllergyModel } from "@/fhir/models/allergies";
+import { compact } from "@/utils/nodash";
 
-export const patientAllergiesColumns: TableColumn<AllergyModel>[] = [
-  {
-    title: "Description",
-    dataIndex: "display",
-  },
-  {
-    title: "Type",
-    dataIndex: "type",
-  },
-  {
-    title: "Status",
-    dataIndex: "clinicalStatus",
-  },
-  {
-    title: "Category",
-    dataIndex: "categories",
-  },
-  {
-    title: "Onset",
-    dataIndex: "onset",
-  },
-  {
-    title: "Manifestations",
-    dataIndex: "manifestations",
-  },
-  {
-    render: (allergy) => (
-      <ViewFHIR name="Allergy Resource" resource={allergy.resource} />
-    ),
-  },
-];
+export const patientAllergiesColumns = (
+  cardView = false
+): TableColumn<AllergyModel>[] =>
+  compact([
+    {
+      title: "Description",
+      dataIndex: "display",
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+    },
+    cardView
+      ? null
+      : {
+          title: "Status",
+          dataIndex: "clinicalStatus",
+        },
+    {
+      title: "Category",
+      dataIndex: "categories",
+    },
+    {
+      title: "Onset",
+      dataIndex: "onset",
+    },
+    cardView
+      ? null
+      : {
+          render: (allergy) => (
+            <ViewFHIR name="Allergy Resource" resource={allergy.resource} />
+          ),
+        },
+  ]);
--- src/components/content/allergies/patient-allergies.tsx
+++ src/components/content/allergies/patient-allergies.tsx
@@ -1,13 +1,20 @@
 import cx from "classnames";
 import { useRef } from "react";
 import { patientAllergiesColumns } from "@/components/content/allergies/patient-allergies-column";
+import { Heading, StackedWrapper } from "@/components/core/ctw-box";
 import { Table } from "@/components/core/table/table";
+import { ViewFHIR } from "@/components/core/view-fhir";
 import { usePatientAllergies } from "@/fhir/allergies";
+import { AllergyModel } from "@/fhir/models/allergies";
 import { useBreakpoints } from "@/hooks/use-breakpoints";
 
 export type PatientAllergiesProps = {
   className?: string;
 };
+
+const viewRecordFHIR = ({ record }: { record: AllergyModel }) => (
+  <ViewFHIR name="Allergy Resource" resource={record.resource} />
+);
 
 export function PatientAllergies({ className }: PatientAllergiesProps) {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -19,28 +26,27 @@ export function PatientAllergies({ className }: PatientAllergiesProps) {
   const { isLoading } = patientAllergiesQuery;
 
   return (
-    <div
-      ref={containerRef}
-      className={cx(
-        "ctw-border ctw-border-solid ctw-border-divider-light ctw-bg-white",
-        className,
-        {
-          "ctw-stacked": breakpoints.sm,
-        }
-      )}
-    >
-      <div className="ctw-items-center ctw-justify-between ctw-py-5 ctw-px-4">
-        <div className="ctw-ml-3 ctw-text-xl ctw-font-medium ctw-text-content-black">
-          Allergies
-        </div>
+    <StackedWrapper className={cx("ctw-patient-medications", className)}>
+      <Heading title="Allergies" />
+      <div
+        ref={containerRef}
+        className={cx(
+          "ctw-border ctw-border-solid ctw-border-divider-light ctw-bg-white",
+          className,
+          {
+            "ctw-stacked": breakpoints.sm,
+          }
+        )}
+      >
         <Table
+          RowActions={breakpoints.sm ? viewRecordFHIR : undefined}
           stacked={breakpoints.sm}
           className="-ctw-mx-px !ctw-rounded-none"
           isLoading={isLoading}
           records={allergies}
-          columns={patientAllergiesColumns}
+          columns={patientAllergiesColumns(breakpoints.sm)}
         />
       </div>
-    </div>
+    </StackedWrapper>
   );
 }
