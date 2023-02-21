1d3be25f55c15b9dbbc70ea5002fba735d041542
Priyank <pmadria@zushealth.com>
2023-02-21 10:52:14 -0600

    storybook

---
+++ src/components/content/document/patient-document.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { mockDocumentGet, mockPatientGet } from "./mocks/requests";
+import { PatientDocumentProps, PatientDocuments } from "./patient-documents";
+import { CTWProvider } from "@/components/core/providers/ctw-provider";
+import { PatientProvider } from "@/components/core/providers/patient-provider";
+import { SYSTEM_ZUS_UNIVERSAL_ID } from "@/fhir/system-urls";
+
+type Props = PatientDocumentProps;
+
+export default {
+  component: PatientDocuments,
+  tags: ["autodocs"],
+  argTypes: {
+    className: {
+      options: ["Blank", "Fixed Width"],
+      control: "select",
+      mapping: {
+        Blank: "",
+        "Fixed Width": "ctw-m-auto ctw-max-w-[600px]",
+      },
+    },
+  },
+  args: {
+    className: "Blank",
+    readOnly: false,
+  },
+  decorators: [
+    (Story, { args }) => (
+      <CTWProvider env="dev" authToken="dummy-token" builderId="b123">
+        <PatientProvider patientID="u12345" systemURL={SYSTEM_ZUS_UNIVERSAL_ID}>
+          <Story args={args} />
+        </PatientProvider>
+      </CTWProvider>
+    ),
+  ],
+} as Meta<Props>;
+
+export const Basic: StoryObj<Props> = {
+  parameters: {
+    msw: [mockPatientGet, mockDocumentGet],
+  },
+};
