8a545462953d98287b7de1203f2c61c9f982684a
Priyank <pmadria@zushealth.com>
2023-02-21 13:52:01 -0600

    update storybook

--- src/components/content/document/patient-document.stories.tsx
+++ src/components/content/document/patient-document.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { mockDocumentGet, mockPatientGet } from "./mocks/requests";
 import { PatientDocumentProps, PatientDocuments } from "./patient-documents";
+import { setupDocumentMocks } from "./story-helpers/mocks/requests";
 import { CTWProvider } from "@/components/core/providers/ctw-provider";
 import { PatientProvider } from "@/components/core/providers/patient-provider";
 import { SYSTEM_ZUS_UNIVERSAL_ID } from "@/fhir/system-urls";
@@ -36,7 +36,5 @@ export default {
 } as Meta<Props>;
 
 export const Basic: StoryObj<Props> = {
-  parameters: {
-    msw: [mockPatientGet, mockDocumentGet],
-  },
+  ...setupDocumentMocks(),
 };
---
+++ src/components/content/document/story-helpers/mocks/requests.ts
@@ -0,0 +1,26 @@
+import { rest } from "msw";
+import { documents } from "./document";
+import { patient } from "./patient";
+
+function mockDocumentRequests() {
+  const mockPatientGet = rest.get(
+    "https://api.dev.zusapi.com/fhir/Patient",
+    // Add ctx.delay(750), delay to show loading, we set this to 750ms to be
+    // less than the default testing-library timeout of 1000ms.
+    (req, res, ctx) => res(ctx.delay(750), ctx.status(200), ctx.json(patient))
+  );
+
+  const mockDocumentGet = rest.get(
+    "https://api.dev.zusapi.com/fhir/DocumentReference",
+    (req, res, ctx) => res(ctx.status(200), ctx.json(documents))
+  );
+  return [mockPatientGet, mockDocumentGet];
+}
+
+export function setupDocumentMocks() {
+  return {
+    parameters: {
+      msw: mockDocumentRequests(),
+    },
+  };
+}
