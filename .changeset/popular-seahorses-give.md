2aefff355dc8d4a360d3b0f7c2b58be517556663
Eric Gunther <97455910+eric-gunther@users.noreply.github.com>
2023-02-21 15:14:41 -0500

    Removing default padding for components (#518)
    [We had previously updated the default padding for
    components](https://github.com/zeus-health/ctw-component-library/commit/385304f13e14b0ae28d602195739abd12449b163#diff-85c539d3d5feacb483cc18d614e7de347974dc61eceae017276d8f7dc24412af).
    After some discussion we believe it makes more sense for consuming apps
    to determine the right amount of padding.

---
+++ .changeset/popular-seahorses-give.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": patch
+---
+
+Remove padding that is added by default.
--- src/components/content/medications/patient-medications-tabbed.tsx
+++ src/components/content/medications/patient-medications-tabbed.tsx
@@ -160,10 +160,7 @@ export function PatientMedicationsTabbed({
 
   return (
     <CTWBox.StackedWrapper
-      className={cx(
-        "ctw-patient-resource-component ctw-patient-medications ctw-space-y-3",
-        className
-      )}
+      className={cx("ctw-patient-medications ctw-space-y-3", className)}
     >
       <TabGroup
         content={tabItems}
