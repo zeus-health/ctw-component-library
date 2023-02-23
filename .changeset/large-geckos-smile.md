f2277a2b0f4109ce3a3d48ef661ad6a82cb1a212
Michael Rosata <mrosata@zushealth.com>
2023-02-23 15:25:22 -0500

    Uniform tables and telemetry (#523)
    Telemetry and error boundary wrapper on Allergies, Immunizations and
    Documents.

---
+++ .changeset/large-geckos-smile.md
@@ -0,0 +1,7 @@
+---
+"@zus-health/ctw-component-library": patch
+---
+
+Telemetry wrapper on Allergies, Immunizations and Documents.
+Remove heading titles from Immunizations and Documents.
+Remove inner padding on PatientsTable.
--- src/components/content/immunizations/patient-immunizations.tsx
+++ src/components/content/immunizations/patient-immunizations.tsx
@@ -2,7 +2,7 @@ import cx from "classnames";
 import { useRef } from "react";
 import { useImmunizationDetailsDrawer } from "./immunizations-details-drawer";
 import { patientImmunizationsColumns } from "./patient-immunizations-columns";
-import { Heading } from "@/components/core/ctw-box";
+import { withErrorBoundary } from "@/components/core/error-boundary";
 import { Table } from "@/components/core/table/table";
 import { ViewFHIR } from "@/components/core/view-fhir";
 import { usePatientImmunizations } from "@/fhir/immunizations";
@@ -17,7 +17,9 @@ const viewRecordFHIR = ({ record }: { record: ImmunizationModel }) => (
   <ViewFHIR name="Immunization Resource" resource={record.resource} />
 );
 
-export function PatientImmunizations({ className }: PatientImmunizationsProps) {
+function PatientImmunizationsComponent({
+  className,
+}: PatientImmunizationsProps) {
   const containerRef = useRef<HTMLDivElement>(null);
   const breakpoints = useBreakpoints(containerRef);
   const patientImmunizationsQuery = usePatientImmunizations();
@@ -30,24 +32,28 @@ export function PatientImmunizations({ className }: PatientImmunizationsProps) {
   return (
     <div
       ref={containerRef}
-      className={cx(
-        "ctw-patient-immunizations ctw-border ctw-border-solid ctw-border-divider-light ctw-bg-white",
-        className,
-        {
-          "ctw-stacked": breakpoints.sm,
-        }
-      )}
+      data-zus-telemetry-namespace="Immunizations"
+      className={cx("ctw-patient-immunizations ctw-bg-white", className, {
+        "ctw-stacked": breakpoints.sm,
+      })}
     >
-      <Heading title="Immunizations" />
-      <Table
-        RowActions={viewRecordFHIR}
-        stacked={breakpoints.sm}
-        className="-ctw-mx-px !ctw-rounded-none"
-        isLoading={patientImmunizationsQuery.isLoading}
-        records={patientImmunizationsQuery.data ?? []}
-        columns={patientImmunizationsColumns}
-        handleRowClick={handleRowClick}
-      />
+      <div className="ctw-overflow-hidden">
+        <Table
+          removeLeftAndRightBorders
+          RowActions={viewRecordFHIR}
+          stacked={breakpoints.sm}
+          className="-ctw-mx-px !ctw-rounded-none"
+          isLoading={patientImmunizationsQuery.isLoading}
+          records={patientImmunizationsQuery.data ?? []}
+          columns={patientImmunizationsColumns}
+          handleRowClick={handleRowClick}
+        />
+      </div>
     </div>
   );
 }
+
+export const PatientImmunizations = withErrorBoundary(
+  PatientImmunizationsComponent,
+  "PatientImmunizations"
+);
