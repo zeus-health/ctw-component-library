fa01384e22a9abde7c4fe10bc42442aa7847e26f
Wesley Reitzfeld <wesley@kapowcorp.com>
2023-03-01 10:40:37 -0500

    Remove column sorting from table

--- src/utils/sort.test.ts
+++ src/utils/sort.test.ts
@@ -1,9 +1,6 @@
-import {
-  IndexSort,
-  sortByIndices,
-} from "@/components/core/table/table-helpers";
+import { applySorts, Sort } from "./sort";
 
-describe("Sort by Indices", () => {
+describe("applySorts", () => {
   type Triplet = { one?: string; two?: string; three?: string };
   const noOne: Triplet = { two: "b", three: "c" };
   const noTwo: Triplet = { one: "a", three: "c" };
@@ -11,16 +8,16 @@ describe("Sort by Indices", () => {
   const sameOneDiffTwo1: Triplet = { one: "same", two: "a" };
   const sameOneDiffTwo2: Triplet = { one: "same", two: "b" };
   const tests = [noOne, noTwo, noThree, sameOneDiffTwo1, sameOneDiffTwo2];
-  const indexSortsAsc: IndexSort<Triplet>[] = [
-    { index: "one", dir: "asc" },
-    { index: "two", dir: "asc" },
+  const indexSortsAsc: Sort<Triplet>[] = [
+    { key: "one", dir: "asc" },
+    { key: "two", dir: "asc" },
   ];
-  const indexSortsDescAsc: IndexSort<Triplet>[] = [
-    { index: "one", dir: "desc" },
-    { index: "two", dir: "asc" },
+  const indexSortsDescAsc: Sort<Triplet>[] = [
+    { key: "one", dir: "desc" },
+    { key: "two", dir: "asc" },
   ];
   test("Sort in ascending order.", () => {
-    expect(sortByIndices(tests, indexSortsAsc)).toEqual([
+    expect(applySorts(tests, indexSortsAsc)).toEqual([
       noThree, // [A, B, _]
       noTwo, // [A, _, C]
       sameOneDiffTwo1, // [same, A, _]
@@ -29,7 +26,7 @@ describe("Sort by Indices", () => {
     ]);
   });
   test("Sort in descending order, then ascending order.", () => {
-    expect(sortByIndices(tests, indexSortsDescAsc)).toEqual([
+    expect(applySorts(tests, indexSortsDescAsc)).toEqual([
       sameOneDiffTwo1, // [same, A, _]
       sameOneDiffTwo2, // [same, B, _]
       noThree, // [A, B, _]
