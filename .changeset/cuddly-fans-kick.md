d5270538a6cbfcec21381c2256d452bc2f63f008
Priyank Madria <98342697+pmadria3@users.noreply.github.com>
2023-03-14 15:57:40 -0500

    Ctw-809/care-team-updates (#577)
    CTW-809
    
    This PR updates care team with each row being an individual practitioner
    and then the drawer having details about the practitioner.

---
+++ .changeset/cuddly-fans-kick.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": minor
+---
+
+Update CareTeams to each row being an individual practitioner and then the drawer having details about the practitioner.
--- src/fhir/models/careteam.ts
+++ src/fhir/models/careteam.ts
@@ -3,49 +3,40 @@ import { formatDateISOToLocal } from "../formatters";
 import { findReference } from "../resource-helper";
 import { FHIRModel } from "./fhir-model";
 import { PractitionerModel } from "./practitioner";
+import { find } from "@/utils/nodash";
 
 export class CareTeamModel extends FHIRModel<fhir4.CareTeam> {
-  get status(): string | undefined {
+  get status() {
     return this.resource.status;
   }
 
-  get periodStart(): string | undefined {
+  get periodStart() {
     return formatDateISOToLocal(this.resource.period?.start);
   }
 
-  get periodEnd(): string | undefined {
+  get periodEnd() {
     return this.resource.period?.end;
   }
 
-  get includedPerformer(): string | undefined {
-    const reference = this.resource.participant?.[0]?.member?.reference;
-
-    const practitioner = findReference(
-      "Practitioner",
-      this.resource.contained,
-      this.includedResources,
-      reference
-    );
-
-    if (practitioner) {
-      return new PractitionerModel(practitioner).fullName;
-    }
-    return this.resource.participant?.[0]?.member?.display;
+  get particpants() {
+    return this.resource.participant;
   }
 
-  get careTeamTelecom(): string | undefined {
-    return this.resource.telecom?.[0].value;
+  get careTeamTelecom() {
+    return this.resource.telecom;
   }
 
-  get managingOrganization(): string | undefined {
-    return this.resource.participant?.[0].onBehalfOf?.display;
+  get managingOrganization() {
+    const behalfOf = find(this.resource.participant, "onBehalfOf")?.onBehalfOf
+      ?.display;
+    return behalfOf;
   }
 
-  get role(): string | undefined {
+  get role() {
     return codeableConceptLabel(this.resource.participant?.[0].role?.[0]);
   }
 
-  get practitionerQualification(): string | undefined {
+  get practitionerQualification() {
     const reference = this.resource.participant?.[0]?.member?.reference;
 
     const practitioner = findReference(
@@ -61,5 +52,14 @@ export class CareTeamModel extends FHIRModel<fhir4.CareTeam> {
       );
     }
     return undefined;
+  }
+
+  getPractitionerByID(reference: string | undefined) {
+    return findReference(
+      "Practitioner",
+      this.resource.contained,
+      this.includedResources,
+      reference
+    );
   }
 }
