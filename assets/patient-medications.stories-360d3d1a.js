a39f5f09622461259aa26f410a2951c473306ab8
mrosata <michael.rosata@gmail.com>
2023-03-16 15:13:14 +0000

    Deploying to gh-pages from  @ bd7be90e7f435b16c26560bcb99c6e2fbb6773a7 🚀

---
+++ assets/patient-medications.stories-360d3d1a.js
@@ -0,0 +1,16 @@
+import{R as i}from"./index-6f814c40.js";import{s as v,p as M,o as f}from"./requests-c380c1df.js";import{P as g}from"./patient-medications-f7defcea.js";import{C as H,S as R}from"./patient-helper-2c738c95.js";import{P}from"./patient-provider-d2b81159.js";import"./_commonjsHelpers-042e6b4d.js";import"./request-5a1df4c1.js";import"./patient-8f4a0ec9.js";import"./values-c30f357d.js";import"./_baseForOwn-56487e0e.js";import"./_equalByTag-3aa7c076.js";import"./_baseIsEqual-4b283a92.js";import"./_baseToString-ba0098b0.js";import"./sortBy-3860efd7.js";import"./_baseClone-0c3bfcab.js";import"./_createSet-3c80ad01.js";import"./toNumber-e7174cd4.js";import"./_basePickBy-a17ae44f.js";import"./mapValues-fd7519e7.js";import"./isPlainObject-8f51cb87.js";import"./uniq-f1be3f26.js";import"./index-74f03c09.js";import"./other-provider-meds-table-8e14e9c3.js";import"./use-medications-64655112.js";import"./data-list-254aa0b0.js";import"./drawer-ed34104d.js";import"./use-watch-a9671586.js";import"./index-6de6b113.js";import"./medication-history-c36c5fa9.js";import"./collapsible-data-list-details-f7571cef.js";import"./document-icon-581c51b2.js";import"./error-boundary-784d2b69.js";import"./use-breakpoints-04232dc9.js";import"./sortBy-64fcb484.js";import"./getPrototypeOf-28a32d6e.js";import"./loading-c7ff698a.js";import"./spinner-66aa4ba7.js";import"./sort-b8885781.js";import"./isString-b8ede3fb.js";import"./uniqWith-ade993ae.js";import"./table-0bb423c6.js";import"./drawer-form-with-fields-5cad16b0.js";import"./isEmpty-ee15a061.js";import"./combobox-field-ad9aa4cf.js";import"./debounce-d7732be0.js";import"./use-controllable-f15d4b9a.js";import"./calculate-active-index-048f6a58.js";import"./use-filtered-sorted-data-9e5fa73c.js";import"./index-4d501b15.js";import"./extends-98964cd2.js";import"./provider-meds-table-3c3b9e6e.js";import"./filter-bar-4f4b1b5d.js";import"./_commonjs-dynamic-modules-302442b1.js";const Pr={tags:["autodocs"],component:g,decorators:[(l,{args:T})=>i.createElement(H,{env:"dev",authToken:"ey.12345",builderId:"12345"},i.createElement(P,{patientID:"007",systemURL:R},i.createElement(l,{args:T})))],args:{hideAddToRecord:void 0,forceHorizontalTabs:void 0,onAfterOpenHistoryDrawer:void 0,onOpenHistoryDrawer:void 0}},r={...v({providerMedications:M,otherProviderMedications:f})},o={...r,args:{forceHorizontalTabs:!0}},t={...r,args:{hideAddToRecord:!0}};var e,a,p;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  ...setupMedicationMocks({
+    providerMedications,
+    otherProviderMedications
+  })
+}`,...(p=(a=r.parameters)==null?void 0:a.docs)==null?void 0:p.source}}};var m,s,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  ...Basic,
+  args: {
+    forceHorizontalTabs: true
+  }
+}`,...(d=(s=o.parameters)==null?void 0:s.docs)==null?void 0:d.source}}};var c,n,u;t.parameters={...t.parameters,docs:{...(c=t.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  ...Basic,
+  args: {
+    hideAddToRecord: true
+  }
+}`,...(u=(n=t.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};const Sr=["Basic","ForceHorizontalTabs","HideAddToRecord"];export{r as Basic,o as ForceHorizontalTabs,t as HideAddToRecord,Sr as __namedExportsOrder,Pr as default};
