1fabb8d1bcfd38af4a4e3f4a425b397914c34fbf
pmadria3 <98342697+pmadria3@users.noreply.github.com>
2023-03-17 17:29:28 +0000

    Deploying to gh-pages from  @ 81e86724d477e020d6fd80c83307ad810dc39f6d 🚀

---
+++ assets/zus-aggregated-profile.stories-b9d2d6d0.js
@@ -0,0 +1,45 @@
+import{R as u}from"./index-6f814c40.js";import{s as b,a as s}from"./allergy-intolerance-a6c7eb15.js";import{s as U}from"./requests-de488e23.js";import{s as x,Z as L,o as t,p as n}from"./requests-15b0739a.js";import{s as B}from"./requests-433e53be.js";import{s as H}from"./requests-f33b2a30.js";import{s as N,o as a,p as m}from"./requests-c5367779.js";import{s as V}from"./requests-49708b6a.js";import{C as W,S as Y}from"./patient-helper-b0cbc320.js";import{P as j}from"./patient-provider-c19758d6.js";import"./_commonjsHelpers-042e6b4d.js";import"./request-5a1df4c1.js";import"./values-0b6ffb91.js";import"./_baseForOwn-d8306f34.js";import"./_equalByTag-aaf39779.js";import"./_baseIsEqual-c150f525.js";import"./_baseToString-4993715b.js";import"./sortBy-919d7262.js";import"./_baseClone-25b1595e.js";import"./_createSet-12ef9b81.js";import"./toNumber-9b8ac844.js";import"./_basePickBy-239377e6.js";import"./mapValues-21907523.js";import"./isPlainObject-8e58b46f.js";import"./uniq-f5468222.js";import"./zus-da29a567.js";import"./use-patient-history-b836ad7c.js";import"./drawer-form-with-fields-07726940.js";import"./drawer-ed34104d.js";import"./index-74f03c09.js";import"./use-watch-a9671586.js";import"./index-6de6b113.js";import"./spinner-66aa4ba7.js";import"./sortBy-be5f7eb4.js";import"./isEmpty-bcd6f1a3.js";import"./observation-3d4b9f3b.js";import"./sort-4aa98f45.js";import"./isString-35d4a3f2.js";import"./getPrototypeOf-9c757e77.js";import"./uniqWith-f1edcb30.js";import"./null-flavor-28785e40.js";import"./combobox-field-60402e8a.js";import"./debounce-535e186e.js";import"./use-controllable-f15d4b9a.js";import"./calculate-active-index-048f6a58.js";import"./resource-details-drawer-3fe435d7.js";import"./document-icon-581c51b2.js";import"./collapsible-data-list-details-f7571cef.js";import"./loading-c7ff698a.js";import"./medication-history-2359b51b.js";import"./error-boundary-624e34ca.js";import"./use-breakpoints-06041e3b.js";import"./isEqual-b08f36b0.js";import"./coding-list-1d7c950f.js";import"./patient-observations-7ec7ef55.js";import"./table-c78fc2eb.js";import"./filter-bar-030d0df7.js";import"./use-filtered-sorted-data-78da08a4.js";import"./index-4d501b15.js";import"./extends-98964cd2.js";import"./other-provider-meds-table-f170dc28.js";import"./use-medications-9d259250.js";import"./data-list-254aa0b0.js";import"./patient-allergies-c52cf2d7.js";import"./patient-careteam-e5816b7f.js";import"./view-fhir-54e8d253.js";import"./requests-b764d593.js";import"./patient-immunizations-c1910286.js";import"./patient-medications-467c67b8.js";import"./provider-meds-table-ee32ad7a.js";import"./patient-timeline-91719899.js";import"./patient-8f4a0ec9.js";import"./_commonjs-dynamic-modules-302442b1.js";function c({allergyIntolerance:d,otherConditions:p,patientConditions:E,providerMedications:Z,otherProviderMedications:T}){const l=b({allergyIntolerance:d}),R=U(),g=x({otherConditions:p,patientConditions:E}),_=B(),D=H(),M=N({providerMedications:Z,otherProviderMedications:T}),O=V();return{decorators:[...l.decorators,...g.decorators,...M.decorators],parameters:{msw:{handlers:{mocks:[...l.parameters.msw.handlers.mocks,...R.parameters.msw.handlers.mocks,...g.parameters.msw.handlers.mocks,..._.parameters.msw.handlers.mocks,...D.parameters.msw.handlers.mocks,...M.parameters.msw.handlers.mocks,...O.parameters.msw.handlers.mocks]}}}}}const ur={tags:["autodocs"],component:L,decorators:[(d,{args:p})=>u.createElement(W,{env:"dev",authToken:"ey.12345",builderId:"12345"},u.createElement(j,{patientID:"007",systemURL:Y},u.createElement(d,{args:p})))],args:{resources:void 0,forceHorizontalTabs:void 0,title:void 0,allergiesProps:void 0,conditionsProps:void 0,conditionsOutsideProps:void 0,documentsProps:void 0,immunizationsProps:void 0,medicationsProps:void 0,medicationsOutsideProps:void 0,removeBranding:void 0}},o={...c({allergyIntolerance:s,otherConditions:t,otherProviderMedications:a,patientConditions:n,providerMedications:m}),args:{resources:["conditions-outside","medications-outside"]}},r={...c({allergyIntolerance:s,otherConditions:t,otherProviderMedications:a,patientConditions:n,providerMedications:m}),args:{resources:["conditions","conditions-outside","medications","medications-outside"]}},e={...c({allergyIntolerance:s,otherConditions:t,otherProviderMedications:a,patientConditions:n,providerMedications:m}),args:{resources:["allergies","conditions","immunizations","documents"]}},i={...c({allergyIntolerance:s,otherConditions:t,otherProviderMedications:a,patientConditions:n,providerMedications:m}),args:{resources:["allergies","conditions","conditions-outside","documents","immunizations","medications","medications-outside","care-team","timelines"]}};var k,v,P;o.parameters={...o.parameters,docs:{...(k=o.parameters)==null?void 0:k.docs,source:{originalSource:`{
+  ...setupZusAggregatedProfileMocks({
+    allergyIntolerance,
+    otherConditions,
+    otherProviderMedications,
+    patientConditions,
+    providerMedications
+  }),
+  args: {
+    resources: ["conditions-outside", "medications-outside"]
+  }
+}`,...(P=(v=o.parameters)==null?void 0:v.docs)==null?void 0:P.source}}};var h,f,C;r.parameters={...r.parameters,docs:{...(h=r.parameters)==null?void 0:h.docs,source:{originalSource:`{
+  ...setupZusAggregatedProfileMocks({
+    allergyIntolerance,
+    otherConditions,
+    otherProviderMedications,
+    patientConditions,
+    providerMedications
+  }),
+  args: {
+    resources: ["conditions", "conditions-outside", "medications", "medications-outside"]
+  }
+}`,...(C=(f=r.parameters)==null?void 0:f.docs)==null?void 0:C.source}}};var A,y,I;e.parameters={...e.parameters,docs:{...(A=e.parameters)==null?void 0:A.docs,source:{originalSource:`{
+  ...setupZusAggregatedProfileMocks({
+    allergyIntolerance,
+    otherConditions,
+    otherProviderMedications,
+    patientConditions,
+    providerMedications
+  }),
+  args: {
+    resources: ["allergies", "conditions", "immunizations", "documents"]
+  }
+}`,...(I=(y=e.parameters)==null?void 0:y.docs)==null?void 0:I.source}}};var z,S,w;i.parameters={...i.parameters,docs:{...(z=i.parameters)==null?void 0:z.docs,source:{originalSource:`{
+  ...setupZusAggregatedProfileMocks({
+    allergyIntolerance,
+    otherConditions,
+    otherProviderMedications,
+    patientConditions,
+    providerMedications
+  }),
+  args: {
+    resources: ["allergies", "conditions", "conditions-outside", "documents", "immunizations", "medications", "medications-outside", "care-team", "timelines"]
+  }
+}`,...(w=(S=i.parameters)==null?void 0:S.docs)==null?void 0:w.source}}};const lr=["OutsideRecords","ConditionsAndMedications","ProblemsAndDocuments","Everything"];export{r as ConditionsAndMedications,i as Everything,o as OutsideRecords,e as ProblemsAndDocuments,lr as __namedExportsOrder,ur as default};
