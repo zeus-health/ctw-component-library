110da99928daccf62d7b13d4d68b940a5b293381
Wesley Reitzfeld <wesley@kapowcorp.com>
2023-03-17 13:51:12 -0400

    Fix medication history stories

--- src/components/content/medications/history/medication-history.stories.tsx
+++ src/components/content/medications/history/medication-history.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { setupMedicationMocks } from "../../story-helpers/mocks/requests";
 import { otherProviderMedications } from "../story-helpers/mocks/other-provider-medications";
 import { providerMedications } from "../story-helpers/mocks/provider-medications";
 import {
@@ -11,6 +10,7 @@ import { CTWProvider } from "@/components/core/providers/ctw-provider";
 import { PatientProvider } from "@/components/core/providers/patient-provider";
 import { MedicationStatementModel } from "@/fhir/models";
 import { SYSTEM_ZUS_UNIVERSAL_ID } from "@/fhir/system-urls";
+import { setupMedicationMocks } from "../story-helpers/mocks/requests";
 
 type Props = MedicationHistoryProps;
 
