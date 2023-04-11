cc82c7184f6cf0ba281269005b5d611f5e960e02
Bryan Knight <bknight@zushealth.com>
2023-04-11 17:27:03 -0400

    linting

--- src/fhir/models/allergies.ts
+++ src/fhir/models/allergies.ts
@@ -24,7 +24,7 @@ export class AllergyModel extends FHIRModel<fhir4.AllergyIntolerance> {
   get display(): string | undefined {
     return codeableConceptLabel(this.resource.code);
   }
-  
+
   get lowercaseDisplay(): string | undefined {
     return this.display ? this.display.toLowerCase() : this.display;
   }
