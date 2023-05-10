06b1279cb6f31af7e9b9fc9bb10134ccdd3aa0d3
luthfib <luthfibustillos@gmail.com>
2023-05-10 10:20:03 -0400

    Add ability to create users

--- src/components/content/forms/actions/user.ts
+++ src/components/content/forms/actions/user.ts
@@ -1,10 +1,12 @@
-import { CreateOrEditConditionFormData } from "./conditions";
 import { getZusApiBaseUrl } from "@/api/urls";
+import { JSONApiReponse } from "@/api/utils/types";
 import { CTWRequestContext } from "@/components/core/providers/ctw-context";
+import { User } from "@/services/auth/users";
+import { errorResponse } from "@/utils/errors";
 import { ctwFetch } from "@/utils/request";
 
 export const editUserPermissions = async (
-  data: CreateOrEditConditionFormData,
+  data: EditUserFormData,
   getRequestContext: () => Promise<CTWRequestContext>
 ): Promise<unknown> => {
   const requestContext = await getRequestContext();
@@ -29,4 +31,75 @@ export const editUserPermissions = async (
   const response = await ctwFetch(url, { body: JSON.stringify(userEditPayload), method: "PATCH" });
 
   return response;
+};
+
+export async function createUser(
+  data: CreateUserFormData,
+  getRequestContext: () => Promise<CTWRequestContext>
+): Promise<JSONApiReponse<User>> {
+  const requestContext = await getRequestContext();
+  const baseUrl = new URL(`${getZusApiBaseUrl(requestContext.env)}/auth/users`);
+
+  const rolesMap: { [key: string]: string } = {
+    "Care Team User": "3d9d633d-245c-430b-866b-83928cd6581e",
+    "Builder Admin": "abe9baeb-c171-494c-9e3b-6fef27ef4214",
+  };
+
+  const createPayload = {
+    data: {
+      type: "auth/users",
+      attributes: {
+        email: data.email,
+        name: data.name,
+        userType: data.userType,
+        sendPasswordResetEmail: true,
+      },
+      relationships: {
+        "auth/roles": {
+          data: {
+            type: "auth/roles",
+            id: rolesMap[data.role],
+          },
+        },
+        "auth/builders": {
+          data: {
+            type: "auth/builders",
+            id: data.builderId,
+          },
+        },
+        // "fhir/practitioner": {
+        //   data: {
+        //     type: "fhir/practitioner",
+        //     id: "{{OptionalPractitionerID}}",
+        //   },
+        // },
+      },
+    },
+  };
+
+  try {
+    const response = await fetch(baseUrl.href, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${requestContext.authToken}`,
+      },
+      body: JSON.stringify(createPayload),
+    });
+
+    return response.json();
+  } catch (e) {
+    throw errorResponse("Failed fetching patients", e);
+  }
+}
+
+export type EditUserFormData = {
+  id: string;
+};
+
+export type CreateUserFormData = {
+  email: string;
+  name: string;
+  userType: string;
+  role: string;
+  builderId: string;
 };
--- src/components/content/forms/schemas/user-schema.tsx
+++ src/components/content/forms/schemas/user-schema.tsx
@@ -1,7 +1,37 @@
+import { z } from "zod";
 import { UserModel } from "@/api/auth/models/users";
 import { FormEntry } from "@/components/core/form/drawer-form-with-fields";
 
-export const getEditingUserData = ({ user, authRoles }: { user: UserModel }): FormEntry[] => [
+export const getAddUserData = ({ user }: { user: UserModel }): FormEntry[] => [
+  {
+    label: "email",
+    field: "email",
+  },
+  {
+    label: "name",
+    field: "name",
+  },
+  {
+    label: "userType",
+    field: "userType",
+  },
+  {
+    label: "role",
+    value: "test",
+    field: "role",
+  },
+  {
+    label: "builderId",
+    field: "builderId",
+  },
+];
+
+export const getEditingUserData = ({
+  user,
+}: {
+  user: UserModel;
+  authRoles?: string[];
+}): FormEntry[] => [
   {
     label: "id",
     value: user.key,
@@ -17,3 +47,11 @@ export const getEditingUserData = ({ user, authRoles }: { user: UserModel }): Fo
     hidden: true,
   },
 ];
+
+export const userSchema = z.object({
+  email: z.string({ required_error: "Email is required." }),
+  name: z.string({ required_error: "Name is required." }),
+  userType: z.enum(["zus", "builder", "individual"]),
+  role: z.enum(["Builder Admin", "Care Team User"]),
+  builderId: z.string({ required_error: "Builder ID is required." }),
+});
