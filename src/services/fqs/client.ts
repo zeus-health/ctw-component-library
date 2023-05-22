de17ac428670830f621234ed09942ca996f74783
Patrick Valle <github@patrickvalle.com>
2023-05-22 08:32:56 -0400

    actually fixing the fqs URL

--- src/services/fqs/client.ts
+++ src/services/fqs/client.ts
@@ -5,7 +5,7 @@ import { Env } from "@/components/core/providers/ctw-provider";
 import { CTW_REQUEST_HEADER } from "@/utils/request";
 
 export const createGraphqlClient = (requestContext: CTWRequestContext) => {
-  const endpoint = `${getZusApiBaseUrl(requestContext.env)}/fqs`;
+  const endpoint = `${getZusApiBaseUrl(requestContext.env)}/fqs/query`;
   return new GraphQLClient(endpoint, {
     headers: {
       authorization: `Bearer ${requestContext.authToken}`,
@@ -14,7 +14,7 @@ export const createGraphqlClient = (requestContext: CTWRequestContext) => {
 };
 
 export function getFetchFromFqs(env: Env, accessToken: string, builderId?: string) {
-  const baseUrl = `${getZusApiBaseUrl(env)}/fqs}`;
+  const baseUrl = `${getZusApiBaseUrl(env)}/fqs`;
 
   const customHeaders: HeadersInit = CTW_REQUEST_HEADER;
   if (builderId) {
