26c0d0e9850b037b67c4c75fd165aac981eee5b1
Priyank Madria <pmadria@zushealth.com>
2023-05-24 09:30:40 -0500

    added enableToggle

--- src/components/content/conditions/patient-conditions-profile.tsx
+++ src/components/content/conditions/patient-conditions-profile.tsx
@@ -2,17 +2,19 @@ import { ZusAggregatedProfile } from "../zus-aggregated-profile/zus-aggregated-p
 
 export type PatientConditionsProfileProps = {
   hideRequestRecords?: boolean;
+  enableFQS?: boolean;
   readOnly?: boolean;
 };
 
 export const PatientConditionsProfile = ({
   hideRequestRecords,
+  enableFQS = false,
   readOnly,
 }: PatientConditionsProfileProps) => (
   <ZusAggregatedProfile
     hideTitle
     resources={["conditions", "conditions-outside"]}
-    conditionsProps={{ readOnly }}
-    conditionsOutsideProps={{ hideRequestRecords, readOnly }}
+    conditionsProps={{ readOnly, enableFQS }}
+    conditionsOutsideProps={{ hideRequestRecords, readOnly, enableFQS }}
   />
 );
