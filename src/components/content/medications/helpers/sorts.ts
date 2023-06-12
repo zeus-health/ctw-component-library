60f0de25298551ced4754442ec978cb433299e47
Michael Rosata <mrosata@zushealth.com>
2023-06-12 12:28:52 -0400

    fixed date sort direction

--- src/components/content/medications/helpers/sorts.ts
+++ src/components/content/medications/helpers/sorts.ts
@@ -12,23 +12,23 @@ export const medicationSortOptions: SortOption<MedicationStatementModel>[] = [
   },
   {
     display: "Last Fill Date (Oldest to Newest)",
-    sorts: [{ key: "lastFillDate", dir: "desc", isDate: true }],
+    sorts: [{ key: "lastFillDate", dir: "asc", isDate: true }],
   },
   {
     display: "Last Fill Date (Newest to Oldest)",
-    sorts: [{ key: "lastFillDate", dir: "asc", isDate: true }],
+    sorts: [{ key: "lastFillDate", dir: "desc", isDate: true }],
   },
   {
     display: "Last Prescribed (Old to New)",
     sorts: [
-      { key: "lastPrescribedDate", isDate: true, dir: "desc" },
+      { key: "lastPrescribedDate", isDate: true, dir: "asc" },
       { key: "lastPrescriber", dir: "asc" },
     ],
   },
   {
     display: "Last Prescribed (New to Old)",
     sorts: [
-      { key: "lastPrescribedDate", isDate: true, dir: "asc" },
+      { key: "lastPrescribedDate", isDate: true, dir: "desc" },
       { key: "lastPrescriber", dir: "asc" },
     ],
   },
