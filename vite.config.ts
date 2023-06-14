dd793ddaf27b94b5b22dc0916133a6aacc43bbcc
Michael Rosata <mrosata@zushealth.com>
2023-06-14 10:26:17 -0400

    install deps

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import { dependencies, peerDependencies } from "./package.json";
 
-const DEPS_TO_BUNDLE = ["graphql-request", "graphql", "@radix-ui/react-dropdown-menu"];
+const DEPS_TO_BUNDLE = ["graphql-request", "graphql"];
 
 export default defineConfig({
   plugins: [
