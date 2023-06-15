a01b388955b401bb6e87ff9e9a2355515e8dba05
Patrick Valle <github@patrickvalle.com>
2023-06-15 16:23:10 -0400

    CDEV-52: Fixing flag ready slowness

--- src/hooks/use-feature-variant.ts
+++ src/hooks/use-feature-variant.ts
@@ -15,22 +15,12 @@ export function useFeatureVariant(name: string): FeatureVariant {
   const status = useFlagsStatus();
   const variant = useVariant(name);
   const [clientError, setClientError] = useState<boolean>();
-  const [clientRefetched, setClientRefetched] = useState<boolean>();
-  const { userId } = client.getContext();
 
   useEffect(() => {
     client.on("error", () => {
       setClientError(true);
     });
   }, [client]);
-
-  useEffect(() => {
-    if (userId) {
-      client.on("update", () => {
-        setClientRefetched(true);
-      });
-    }
-  }, [client, userId]);
 
   // return "ready" if unleash failed to bootstrap
   if (clientError) {
@@ -39,10 +29,6 @@ export function useFeatureVariant(name: string): FeatureVariant {
   // return "ready" if unleash failed to load flags
   if (status.flagsError) {
     return { ready: true };
-  }
-  // return "not ready" if unleash hasn't refetched flags after the context changed.
-  if (!clientRefetched) {
-    return { ready: false };
   }
   // return "not ready" if unleash is still fetching flags
   if (!status.flagsReady && !status.flagsError) {
