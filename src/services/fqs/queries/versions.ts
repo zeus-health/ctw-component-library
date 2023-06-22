773d86c48047dbb073a2917623b640e523e623e0
Priyank Madria <98342697+pmadria3@users.noreply.github.com>
2023-06-22 09:14:41 -0500

    Ctw 1200/rewire use history (#831)
    This PR sets up the drawer to retrieve the versions history for
    resources (currently conditions) and also rewires the useHistory hook to
    utilize FQS for resources.
    
    ---------

---
+++ src/services/fqs/queries/versions.ts
@@ -0,0 +1,37 @@
+import { gql } from "graphql-request";
+import { fragmentCondition } from "./fragments/condition";
+import { ResourceTypeString } from "@/fhir/types";
+
+export function versionsQuery(resourceType: ResourceTypeString, resourceIds: string[]) {
+  const fragment = getResourceFragment(resourceType);
+
+  const queries = resourceIds
+    .map((resourceId, ind) => createHistoryQuery(resourceType, resourceId, ind))
+    .join("\n");
+
+  return gql`
+  ${fragment}
+    query historyQuery {
+      ${queries}
+    }
+  `;
+}
+
+function getResourceFragment(resourceType: ResourceTypeString) {
+  switch (resourceType) {
+    case "Condition":
+      return fragmentCondition(true);
+    default:
+      throw new Error(`Resource type to FQS query not implemented yet for ${resourceType}`);
+  }
+}
+
+function createHistoryQuery(resourceType: ResourceTypeString, resourceId: string, ind: number) {
+  return `
+    history${ind}: ${resourceType}History(
+        id: "${resourceId}"
+      ) {          
+        ...${resourceType}
+      }
+  `;
+}
