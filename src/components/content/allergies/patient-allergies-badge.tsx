c731a69858da6214c87082e394987435e20a61ff
Eric Gunther <egunther@zushealth.com>
2023-06-30 07:52:28 -0400

    fixing builder ID

--- src/components/content/allergies/patient-allergies-badge.tsx
+++ src/components/content/allergies/patient-allergies-badge.tsx
@@ -1,12 +1,25 @@
+import { useEffect, useState } from "react";
 import { Badge } from "@/components/core/badge";
 import { useCTW } from "@/components/core/providers/use-ctw";
 import { usePatientAllergies } from "@/fhir/allergies";
 
 export const PatientAllergiesBadge = () => {
   const query = usePatientAllergies();
-  const { builderId } = useCTW();
+  const { getRequestContext } = useCTW();
+
+  const [userBuilderId, setUserBuilderId] = useState("");
+
+  useEffect(() => {
+    async function load() {
+      const requestContext = await getRequestContext();
+      setUserBuilderId(requestContext.builderId);
+    }
+
+    void load();
+  }, [getRequestContext]);
+
   const unreadOutsideAllergies = query.data.filter(
-    (allergy) => !allergy.isDismissed && !allergy.isRead && !allergy.ownedByBuilder(builderId)
+    (allergy) => !allergy.isDismissed && !allergy.isRead && !allergy.ownedByBuilder(userBuilderId)
   );
 
   if (unreadOutsideAllergies.length > 0) {
