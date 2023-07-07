c4976b13fe4ff20291b9b65615f4026b20fa9224
Eric Gunther <97455910+eric-gunther@users.noreply.github.com>
2023-07-07 15:30:42 -0400

    CTW-1282 Add className input for UnreadNotification icon (#929)
    In the standalone app we sometimes need to change the color of this icon
    so adding a classname input.

--- src/components/core/unread-notification.tsx
+++ src/components/core/unread-notification.tsx
@@ -1,10 +1,11 @@
-export const UnreadNotification = () => (
-  <svg
-    height={10}
-    className="ctw-fill-notification-icon"
-    viewBox="0 0 10 10"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+export type UnreadNotificationProps = {
+  className?: string;
+};
+
+export const UnreadNotification = ({
+  className = "ctw-fill-notification-icon",
+}: UnreadNotificationProps) => (
+  <svg height={10} className={className} viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
     <circle cx="5" cy="5" r="5" />
   </svg>
 );
