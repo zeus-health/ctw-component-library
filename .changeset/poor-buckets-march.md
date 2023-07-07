2b48fb362fa4c348d908e77226406de6d4cfaea5
Eric Gunther <egunther@zushealth.com>
2023-07-07 14:26:11 -0400

    add classname to unread notification

---
+++ .changeset/poor-buckets-march.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": patch
+---
+
+Add className for UnreadNotification component.
--- src/components/core/unread-notification.tsx
+++ src/components/core/unread-notification.tsx
@@ -1,7 +1,7 @@
-export const UnreadNotification = () => (
+export const UnreadNotification = (className?: string) => (
   <svg
     height={10}
-    className="ctw-fill-notification-icon"
+    className={className ?? "ctw-fill-notification-icon"}
     viewBox="0 0 10 10"
     xmlns="http://www.w3.org/2000/svg"
   >
