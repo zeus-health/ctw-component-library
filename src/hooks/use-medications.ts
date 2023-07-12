373b37230161d5bd32c539ffe5bc8d3d39479712
Eric Gunther <egunther@zushealth.com>
2023-07-12 16:48:58 -0400

    removing comment

--- src/hooks/use-medications.ts
+++ src/hooks/use-medications.ts
@@ -68,7 +68,6 @@ export function useQueryAllPatientMedications() {
   const summarizedMedicationsQuery = useQueryGetSummarizedPatientMedications();
   const builderMedicationsQuery = useQueryGetPatientMedsForBuilder();
 
-  // refactor to use new hook!
   useEffect(() => {
     if (
       fqs.ready &&
