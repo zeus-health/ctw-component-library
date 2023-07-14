dcdfa0c98b085d2cd6cd3764c133de5f537e7ecc
Patrick Valle <github@patrickvalle.com>
2023-07-14 13:23:36 -0400

    more

--- src/components/content/observations/helpers/columns.tsx
+++ src/components/content/observations/helpers/columns.tsx
@@ -1,6 +1,7 @@
 import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cx from "classnames";
+import { useState } from "react";
 import { BubbleIcon } from "./bubble";
 import { TableColumn } from "@/components/core/table/table-helpers";
 import { DiagnosticReportModel, ObservationModel } from "@/fhir/models";
@@ -37,80 +38,94 @@ export const diagnosticReportColumns: TableColumn<DiagnosticReportModel>[] = [
   },
 ];
 
-export const observationsColumns: TableColumn<ObservationModel>[] = [
-  {
-    title: "Component",
-    render: (model) => <div className="ctw-flex ctw-flex-wrap ctw-text-base">{model.display}</div>,
-  },
-  {
-    title: "Result",
-    render: (model) => (
-      <div className="ctw-flex">
-        {model.interpretation
-          ? model.interpretation &&
-            model.value && (
-              <div className="ctw-text-sm ctw-font-normal">
-                Result:{" "}
-                <BubbleIcon
-                  interpretation={model.interpretation}
-                  result={model.value}
-                  className={model.acceptedInterpretations}
-                />
-              </div>
-            )
-          : model.value && (
-              <div className="ctw-text-sm ctw-font-normal">
-                Result:{" "}
-                <BubbleIcon result={model.value} className={model.acceptedInterpretations} />
-              </div>
-            )}
-      </div>
-    ),
-  },
-  {
-    title: "Reference Range",
-    render: (model) => (
-      <div className="ctw-flex ctw-text-sm ctw-font-normal">
-        {model.referenceRange && (
-          <div className="ctw-font-normal">
-            Reference Range: {model.referenceRange} {model.unit}
-          </div>
-        )}
-      </div>
-    ),
-  },
-  {
-    title: "Trends",
-    render: (model) => (
-      <div className="ctw-flex ctw-text-sm ctw-font-normal">
-        {model.trends.length > 1 && (
-          <div className="ctw-pt-1">
-            <button type="button" className="ctw-btn-clear">
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                className={cx("ctw-h-3 ctw-w-3 ctw-pr-1", {
-                  "ctw-rotate-90": false,
-                })}
-              />
-              Trends
-            </button>
-            {model.trends.map((trend) => {
-              let fontWeight = "";
-              if (trend.id === model.id) {
-                fontWeight = "ctw-font-bold";
-              }
-              return (
-                <div key={trend.id} className="ctw-relative ctw-clear-both ctw-columns-2 ctw-py-px">
-                  <div className={`${fontWeight} ctw-relative ctw-top-1 ctw-text-sm`}>
-                    {trend.effectiveStart}
-                  </div>
-                  <BubbleIcon result={model.value} className={trend.acceptedInterpretations} />
+export const ObservationsColumns = (): TableColumn<ObservationModel>[] => {
+  const [isTrendsShown, setIsTrendsShown] = useState(false);
+  return [
+    {
+      title: "Component",
+      render: (model) => (
+        <div className="ctw-flex ctw-flex-wrap ctw-text-base">{model.display}</div>
+      ),
+    },
+    {
+      title: "Result",
+      render: (model) => (
+        <div className="ctw-flex">
+          {model.interpretation
+            ? model.interpretation &&
+              model.value && (
+                <div className="ctw-text-sm ctw-font-normal">
+                  Result:{" "}
+                  <BubbleIcon
+                    interpretation={model.interpretation}
+                    result={model.value}
+                    className={model.acceptedInterpretations}
+                  />
                 </div>
-              );
-            })}
-          </div>
-        )}
-      </div>
-    ),
-  },
-];
+              )
+            : model.value && (
+                <div className="ctw-text-sm ctw-font-normal">
+                  Result:{" "}
+                  <BubbleIcon result={model.value} className={model.acceptedInterpretations} />
+                </div>
+              )}
+        </div>
+      ),
+    },
+    {
+      title: "Reference Range",
+      render: (model) => (
+        <div className="ctw-flex ctw-text-sm ctw-font-normal">
+          {model.referenceRange && (
+            <div className="ctw-font-normal">
+              Reference Range: {model.referenceRange} {model.unit}
+            </div>
+          )}
+        </div>
+      ),
+    },
+    {
+      title: "Trends",
+      render: (model) => (
+        <div className="ctw-flex ctw-text-sm ctw-font-normal">
+          {model.trends.length > 1 && (
+            <div className="ctw-pt-1">
+              <button
+                aria-label="trends"
+                className="ctw-btn-clear"
+                type="button"
+                onClick={() => setIsTrendsShown(!isTrendsShown)}
+              >
+                <FontAwesomeIcon
+                  icon={faChevronRight}
+                  className={cx("ctw-h-3 ctw-w-3 ctw-pr-1", {
+                    "ctw-rotate-90": isTrendsShown,
+                  })}
+                />
+                Trends
+              </button>
+              {isTrendsShown &&
+                model.trends.map((trend) => {
+                  let fontWeight = "";
+                  if (trend.id === model.id) {
+                    fontWeight = "ctw-font-bold";
+                  }
+                  return (
+                    <div
+                      key={trend.id}
+                      className="ctw-relative ctw-clear-both ctw-columns-2 ctw-py-px"
+                    >
+                      <div className={`${fontWeight} ctw-relative ctw-top-1 ctw-text-sm`}>
+                        {trend.effectiveStart}
+                      </div>
+                      <BubbleIcon result={model.value} className={trend.acceptedInterpretations} />
+                    </div>
+                  );
+                })}
+            </div>
+          )}
+        </div>
+      ),
+    },
+  ];
+};
