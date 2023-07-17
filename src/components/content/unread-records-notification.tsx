bd191657d54d40b20c302ad4f21a7156091cce30
Eric Gunther <97455910+eric-gunther@users.noreply.github.com>
2023-07-17 18:10:27 -0400

    CTW-1294 General unread records notification (#955)
    Adds a new component `UnreadRecordsNotification` that displays the
    notification if there are any unread records across multiple resource
    types.
    
    There is some duplication across this component and the existing
    `UnreadConditionsNotification`, `UnreadMedicationsNotification`, etc. We
    could get rid of those and just use this one but I don't think the
    proliferation of these different unread notifications is all that bad or
    confusing.
    
    
    
    https://github.com/zeus-health/ctw-component-library/assets/97455910/06648a7a-35f0-49aa-b042-15d6e72115fd
    
    ---------
    
    Co-authored-by: Michael Rosata <mrosata@zushealth.com>

---
+++ src/components/content/unread-records-notification.tsx
@@ -0,0 +1,66 @@
+import fhir4 from "fhir/r4";
+import { ZAPResourceName } from "./zus-aggregated-profile/zus-aggregated-profile";
+import { useUserBuilderId } from "../core/providers/user-builder-id";
+import { UnreadNotificationIcon } from "../core/unread-notification-icon";
+import { usePatientAllergies } from "@/fhir/allergies";
+import { usePatientAllDiagnosticReports } from "@/fhir/diagnostic-report";
+import { usePatientDocuments } from "@/fhir/document";
+import { usePatientImmunizations } from "@/fhir/immunizations";
+import { FHIRModel } from "@/fhir/models/fhir-model";
+import { useQueryAllPatientMedications } from "@/hooks/use-medications";
+import { usePatientConditionsAll } from "@/services/conditions";
+
+export type UnreadRecordsNotificationProps = {
+  className?: string;
+  resources: ZAPResourceName[];
+  text?: string;
+};
+
+export const UnreadRecordsNotification = ({
+  className,
+  resources,
+  text,
+}: UnreadRecordsNotificationProps) => {
+  const userBuilderId = useUserBuilderId();
+  const allergiesQuery = usePatientAllergies();
+  const conditionsQuery = usePatientConditionsAll();
+  const diagnosticReportsQuery = usePatientAllDiagnosticReports();
+  const documentsQuery = usePatientDocuments();
+  const immunizationsQuery = usePatientImmunizations();
+  const medicationsQuery = useQueryAllPatientMedications();
+
+  for (const resource of resources) {
+    let data: FHIRModel<fhir4.Resource>[];
+    switch (resource) {
+      case "allergies":
+        data = allergiesQuery.data;
+        break;
+      case "conditions-all":
+        data = conditionsQuery.data;
+        break;
+      case "diagnostic-reports":
+        data = diagnosticReportsQuery.data;
+        break;
+      case "documents":
+        data = documentsQuery.data;
+        break;
+      case "immunizations":
+        data = immunizationsQuery.data;
+        break;
+      case "medications-all":
+        data = medicationsQuery.allMedications;
+        break;
+      default:
+        data = [];
+    }
+    if (
+      data.some(
+        (record) => !record.isDismissed && !record.isRead && !record.ownedByBuilder(userBuilderId)
+      )
+    ) {
+      return <UnreadNotificationIcon className={className} text={text} />;
+    }
+  }
+
+  return null;
+};
