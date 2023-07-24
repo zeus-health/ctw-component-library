c57bc0e2495c8de33cd411a4549a8812ef5e2f34
sarahmangels <95938276+sarahmangels@users.noreply.github.com>
2023-07-24 13:49:09 -0400

    Display records never requested (#1020)

--- src/components/content/patient-history/patient-history-last-retrieved.tsx
+++ src/components/content/patient-history/patient-history-last-retrieved.tsx
@@ -1,3 +1,5 @@
+import { faRotate } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { usePatientHistory } from "./use-patient-history";
 import { formatISODateStringToDate } from "@/fhir/formatters";
 
@@ -6,10 +8,15 @@ export const PatientHistoryLastRetrieved = () => {
 
   return (
     <>
-      {patientHistory.lastRetrievedAt && (
+      {patientHistory.lastRetrievedAt ? (
         <div className="ctw-space-x-1 ctw-text-sm ctw-italic ctw-text-black">
           <span>Last Retrieved</span>
           <span>{formatISODateStringToDate(patientHistory.lastRetrievedAt)}</span>
+        </div>
+      ) : (
+        <div className="ctw-space-x-1 ctw-text-sm ctw-italic ctw-text-black">
+          <FontAwesomeIcon icon={faRotate} className="ctw-h-3" />
+          <span>Records never requested</span>
         </div>
       )}
     </>
