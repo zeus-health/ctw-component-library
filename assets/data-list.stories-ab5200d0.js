5976f54968287bd3b575e63978a56fec41b3ce5d
eric-gunther <97455910+eric-gunther@users.noreply.github.com>
2023-07-24 14:21:59 +0000

    Deploying to gh-pages from  @ 630f2e6cffbe4fcc22ab8f4b6eb5e3b27592f7d8 🚀

---
+++ assets/data-list.stories-ab5200d0.js
@@ -0,0 +1,15 @@
+import{D as n}from"./data-list-2854f463.js";import"./index-9f32f44c.js";import"./_commonjsHelpers-de833af9.js";const i={component:n,tags:["autodocs"]},a={args:{title:"data list title",data:[{label:"String value",value:"hello world"},{label:"Array value",value:["One","two","3"]},{label:"Undefined value",value:void 0}]}};var e,t,l;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    title: "data list title",
+    data: [{
+      label: "String value",
+      value: "hello world"
+    }, {
+      label: "Array value",
+      value: ["One", "two", "3"]
+    }, {
+      label: "Undefined value",
+      value: undefined
+    }]
+  }
+}`,...(l=(t=a.parameters)==null?void 0:t.docs)==null?void 0:l.source}}};const d=["Basic"];export{a as Basic,d as __namedExportsOrder,i as default};
--- assets/uniq-a8ac11c1.js
+++
@@ -1,1 +0,0 @@
-import{_ as i}from"./_baseUniq-b2927ba5.js";var e=i;function t(n){return n&&n.length?e(n):[]}var o=t;export{o as u};
