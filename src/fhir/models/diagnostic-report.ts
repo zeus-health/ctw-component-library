7c75a85448f94b71cb854b2b74d66b196cbcf4f2
Priyank Madria <98342697+pmadria3@users.noreply.github.com>
2023-07-27 15:41:44 -0500

    Ctw 286/lipid trends (#1039)
    CDEV-286
    
    This PR adds trends to selected Lipid Panels. 
    ![Screenshot 2023-07-27 at 2 30 45
    PM](https://github.com/zeus-health/ctw-component-library/assets/98342697/f3a424cf-6789-4fad-8aca-4da6df0e4f6e)

--- src/fhir/models/diagnostic-report.ts
+++ src/fhir/models/diagnostic-report.ts
@@ -32,6 +32,7 @@ export class DiagnosticReportModel extends FHIRModel<fhir4.DiagnosticReport> {
         const model = new ObservationModel(result.resource, {
           [resourceId]: resource,
         });
+
         if (trends) {
           model.trends = filterAndSortTrends(model, trends);
           if (model.trends.length >= 2) {
