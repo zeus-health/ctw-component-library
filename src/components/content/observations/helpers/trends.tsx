980da9d5a07bebff2e62f17da28923ff2086f897
Priyank Madria <pmadria@zushealth.com>
2023-07-31 16:44:14 -0500

    styling

--- src/components/content/observations/helpers/trends.tsx
+++ src/components/content/observations/helpers/trends.tsx
@@ -21,7 +21,7 @@ export const ObservationTrends = ({ model }: ObservationTrendsProps) => {
         <div className="ctw-pt-2">
           <button
             aria-label="trends"
-            className="ctw-btn-clear"
+            className="ctw-btn-clear ctw-space-x-3"
             type="button"
             onClick={() => {
               Telemetry.trackInteraction(
@@ -30,7 +30,8 @@ export const ObservationTrends = ({ model }: ObservationTrendsProps) => {
               setIsTrendsShown(!isTrendsShown);
             }}
           >
-            <FontAwesomeIcon icon={faArrowTrendUp} /> Trends
+            <FontAwesomeIcon icon={faArrowTrendUp} />
+            <span>Trends</span>
             <FontAwesomeIcon
               icon={faChevronRight}
               className={cx("ctw-h-3 ctw-w-3 ctw-pr-1", {
@@ -44,12 +45,9 @@ export const ObservationTrends = ({ model }: ObservationTrendsProps) => {
                 tabIndex={0}
                 role="button"
                 key={trend.id}
-                className={cx(
-                  "ctw-group/trends ctw-flex ctw-items-center ctw-justify-between ctw-py-1 ctw-pl-4",
-                  {
-                    "ctw-cursor-pointer hover:ctw-bg-bg-lighter": trend.diagnosticReport,
-                  }
-                )}
+                className={cx("ctw-group/trends ctw-grid ctw-grid-cols-3 ctw-py-1 ctw-pl-4", {
+                  "ctw-cursor-pointer hover:ctw-bg-bg-lighter": trend.diagnosticReport,
+                })}
                 onClick={() =>
                   trend.diagnosticReport && openDiagnosticReportDetails(trend.diagnosticReport)
                 }
@@ -70,13 +68,13 @@ export const ObservationTrends = ({ model }: ObservationTrendsProps) => {
                 <BubbleIcon
                   result={trend.value}
                   interpretation={trend.interpretation}
-                  className={cx(`${trend.acceptedInterpretations}`, {
+                  className={cx(`${trend.acceptedInterpretations} ctw-w-fit`, {
                     "!ctw-font-bold": trend.id === model.id,
                   })}
                 />
 
                 {trend.diagnosticReport && (
-                  <div>
+                  <div className="ctw-justify-self-end">
                     <FontAwesomeIcon
                       icon={faArrowRight}
                       className="ctw-invisible ctw-pr-2 ctw-text-content-lighter group-hover/trends:ctw-visible"
