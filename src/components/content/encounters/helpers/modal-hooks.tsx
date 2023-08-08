11f0ebddd0282bcc491314d9020ef0dfc2b271cb
Melody Liu <mliu@zushealth.com>
2023-08-08 17:53:44 -0400

    Undid some more unnecessary code

--- src/components/content/encounters/helpers/modal-hooks.tsx
+++ src/components/content/encounters/helpers/modal-hooks.tsx
@@ -1,23 +1,15 @@
 import { useResourceDetailsDrawer } from "../../resource/resource-details-drawer";
 import { CodingList } from "@/components/core/coding-list";
-import { RowActionsProp } from "@/components/core/table/table-rows";
 import { EncounterModel } from "@/fhir/models/encounter";
 import { capitalize } from "@/utils/nodash/fp";
 
-export function usePatientEncounterDetailsDrawer({
-  RowActions,
-  enableDismissAndReadActions,
-}: {
-  RowActions?: RowActionsProp<EncounterModel>;
-  enableDismissAndReadActions?: boolean;
-}) {
+export function usePatientEncounterDetailsDrawer() {
   return useResourceDetailsDrawer({
     header: (m) => `${m.periodStart} - ${m.periodEnd}`,
     subHeader: (m) => m.typeDisplay,
     getSourceDocument: true,
     details: encounterData,
-    RowActions,
-    enableDismissAndReadActions,
+    enableDismissAndReadActions: true,
   });
 }
 
