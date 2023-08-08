f64050f2dc5d0bd639aa8e1905068569209bf696
Logan Brown <lbrown@zushealth.com>
2023-08-08 12:18:48 -0400

    This appears to fix table rendering in firefox

--- src/components/core/table/table-colgroup.tsx
+++ src/components/core/table/table-colgroup.tsx
@@ -10,7 +10,7 @@ export const TableColGroup = <T extends MinRecordItem>({ columns }: TableColGrou
       <col
         key={column.title ?? index}
         className={column.className}
-        width={column.widthPercent ? `${column.widthPercent}%` : "0*"}
+        {column.widthPercent ? `width=${column.widthPercent}%` : ""}
         style={{
           minWidth: column.minWidth,
         }}
