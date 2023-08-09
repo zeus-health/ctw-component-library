69338e37b506e8e84908b35e84784061c82af689
sarahmangels <95938276+sarahmangels@users.noreply.github.com>
2023-08-09 10:47:40 -0400

    CDEV-317 Update date range and remove type field for encounters details drawer (#1099)
    ![image](https://github.com/zeus-health/ctw-component-library/assets/95938276/a0f2826f-f6a5-46af-89f9-95ae7a45a5b4)

---
+++ .changeset/gold-plants-divide.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": minor
+---
+
+Update date range and remove type field for encounters details drawer
--- src/components/content/encounters/helpers/modal-hooks.tsx
+++ src/components/content/encounters/helpers/modal-hooks.tsx
@@ -1,11 +1,21 @@
 import { useResourceDetailsDrawer } from "../../resource/resource-details-drawer";
-import { CodingList } from "@/components/core/coding-list";
 import { EncounterModel } from "@/fhir/models/encounter";
 import { capitalize } from "@/utils/nodash/fp";
 
 export function usePatientEncounterDetailsDrawer() {
   return useResourceDetailsDrawer({
-    header: (m) => `${m.periodStart} - ${m.periodEnd}`,
+    header: (m) => {
+      if (m.periodStart && m.periodEnd) {
+        return `${m.periodStart} - ${m.periodEnd}`;
+      }
+      if (m.periodStart) {
+        return m.periodStart;
+      }
+      if (m.periodEnd) {
+        return m.periodEnd;
+      }
+      return undefined;
+    },
     subHeader: (m) => m.typeDisplay,
     getSourceDocument: true,
     details: encounterData,
@@ -18,12 +28,6 @@ export const encounterData = (encounter: EncounterModel) => [
   { label: "Period End", value: encounter.periodEnd },
   { label: "Status", value: capitalize(encounter.status) },
   { label: "Class", value: encounter.class },
-  {
-    label: "Type",
-    value: encounter.typeCodings.length ? (
-      <CodingList codings={encounter.typeCodings} />
-    ) : undefined,
-  },
   { label: "Location", value: encounter.location },
   { label: "Participants", value: encounter.participants },
   { label: "Reason", value: encounter.reason },
