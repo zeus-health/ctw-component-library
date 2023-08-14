68963590b8a0315cbca388555a6ded5eb4a2d451
Melody Liu <mliu@zushealth.com>
2023-08-14 11:56:48 -0400

    Fix dropdown alignment glitch that appears in CTW

--- src/components/core/tab-group/tab-group.tsx
+++ src/components/core/tab-group/tab-group.tsx
@@ -162,7 +162,7 @@ function TabGroupComponent({
               )}
               optionsClassName={cx(
                 "ctw-tab-list ctw-capitalize",
-                !showTopRightContent && shownContent.length > 1 && "ctw-right-0" // Right-align dropdown if it is rightmost (and not leftmost)
+                !showTopRightContent && content.length > 1 && "ctw-right-0" // Right-align dropdown if it is rightmost (and not leftmost)
               )}
               onChange={(index) => handleOnChange(index + tabOverflowCutoff)}
               items={shownContent}
