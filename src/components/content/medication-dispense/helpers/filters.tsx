ef14e84ee50c8f16dd511ced7e4c7b86d8833098
Priyank Madria <pmadria@zushealth.com>
2023-08-17 09:07:17 -0500

    changes

--- src/components/content/medication-dispense/helpers/filters.tsx
+++ src/components/content/medication-dispense/helpers/filters.tsx
@@ -1,7 +1,6 @@
 import { MedicationDispenseModel } from "@/fhir/models";
 
-export const applyMedDispenseFilters = (data: fhir4.MedicationDispense[]) => {
-  const medDispenseModel = data.map((m) => new MedicationDispenseModel(m));
-
-  return medDispenseModel.filter((m) => m.PMARefillNotPickedUp === "PMARefillNotPickedUp");
-};
+export const applyMedDispenseFilters = (data: fhir4.MedicationDispense[]) =>
+  data
+    .map((m) => new MedicationDispenseModel(m))
+    .filter((m) => m.PMARefillNotPickedUp === "PMARefillNotPickedUp");
