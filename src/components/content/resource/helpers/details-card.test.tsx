79e61111452fe1efd209661f1e38922ebd1b1010
Bryan J. Knight <bknight@zushealth.com>
2023-09-12 14:59:52 -0400

    passing unit tests

---
+++ src/components/content/resource/helpers/details-card.test.tsx
@@ -0,0 +1,260 @@
+/* eslint-disable react/void-dom-elements-no-children */
+import { recursivelyTransposeTables } from "./details-card";
+
+// NOTES:
+// 1. void-dom-elements-no-children is enabled for the file to make the tests happy. This might be an artifact
+//    of how `html-react-parser` works
+// 2. modifications to the expected or the div content is whitespace sensitive
+
+const smallDivContent = `<table>
+  <thead>
+    <tr>
+      <th>Col 1</th>
+      <th>Col 2</th>
+      <th>Col 3</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>hi</td>
+      <td>hi again</td>
+      <td>hi again again</td>
+    </tr>
+  </tbody>
+</table>`;
+
+const largeDivContent = `<div><ul><li><h2>Consultation (Within 1 month) - Closed</h2><table>
+      <thead>
+        <tr>
+          <th>Specialty</th>
+          <th>Diagnoses / Procedures</th>
+          <th>Referred By Contact</th>
+          <th>Referred To Contact</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Neurology</td>
+          <td><p>Diagnoses</p><p>Muscular dystrophy.</p><br /></td>
+          <td><p>Knight, Bryan J, DO</p><p>1 Lincolon St</p><p>Ste 2400</p><p>Boston, MA 02111</p><p>Phone: 508-555-5555</p><p>Fax: 781-555-5555</p><p>Email: bknight@zushealth.com</p></td>
+          <td><p>Acme Health</p><p>200 Jefferson St</p><p>Newton, MA 02462-1607</p><p>Phone: 617-555-5555</p></td>
+        </tr>
+      </tbody>
+    </table><table>
+      <thead>
+        <tr>
+          <th>Referral ID</th>
+          <th>Status</th>
+          <th>Reason</th>
+          <th>Start Date</th>
+          <th>Expiration Date</th>
+          <th>Visits Requested</th>
+          <th>Visits Authorized</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>789456789456</td>
+          <td>Closed</td>
+          <td></td>
+          <td>12/25/2022</td>
+          <td>12/25/2022</td>
+          <td>1</td>
+          <td>1</td>
+        </tr>
+      </tbody>
+    </table><table>
+      <thead>
+        <tr>
+          <th>Scheduling Instructions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td><p>Office to Office is live in Neurology for the following practice:</p><p>Acme Health Neurology Associates, P.C.</p></td>
+        </tr>
+      </tbody>
+    </table><br /><p>Electronically signed by Bryan J Knight DO at 12/25/2022 12:39 PM EDT</p></li></ul></div>`;
+
+describe("detail card tests", () => {
+  describe("transpose", () => {
+    const tests = [
+      {
+        skip: true,
+        name: "one table, one data row, no thead row",
+        value: (
+          <div>
+            <table>
+              <thead>
+                <th>Col 1</th>
+                <th>Col 2</th>
+                <th>Col 3</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>hi</td>
+                  <td>hi again</td>
+                  <td>hi again again</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ),
+        expected: (
+          <div>
+            <table>
+              <tbody>
+                <tr key="transposed-0">
+                  <td key="col-0">Col 1</td>
+                  <td key="data-0-0">hi</td>
+                </tr>
+                <tr key="transposed-1">
+                  <td key="col-1">Col 2</td>
+                  <td key="data-1-1">hi again</td>
+                </tr>
+                <tr key="transposed-2">
+                  <td key="col-2">Col 3</td>
+                  <td key="data-2-2">hi again again</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ),
+      },
+      {
+        name: "one table, one data row, thead row, content",
+        value: <div content={smallDivContent} />,
+        expected: (
+          <div>
+            <table>
+              <tbody>
+                <tr key="transposed-0">
+                  <td key="col-0">Col 1</td>
+                  <td key="data-0-0">hi</td>
+                </tr>
+                <tr key="transposed-1">
+                  <td key="col-1">Col 2</td>
+                  <td key="data-1-1">hi again</td>
+                </tr>
+                <tr key="transposed-2">
+                  <td key="col-2">Col 3</td>
+                  <td key="data-2-2">hi again again</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        ),
+      },
+      {
+        name: "three tables, one data row, thead row, content",
+        value: <div content={largeDivContent} />,
+        expected: (
+          <div>
+            <div>
+              <ul>
+                <li>
+                  <h2 key={0}>Consultation (Within 1 month) - Closed</h2>
+                  <table>
+                    <tbody>
+                      <tr key="transposed-0">
+                        <td key="col-0">Specialty</td>
+                        <td key="data-0-0">Neurology</td>
+                      </tr>
+                      <tr key="transposed-1">
+                        <td key="col-1">Diagnoses / Procedures</td>
+                        <td key="data-1-1">
+                          <p key="0">Diagnoses</p>
+                          <p key="1">Muscular dystrophy.</p>
+                          <br key="2">{null}</br>
+                        </td>
+                      </tr>
+                      <tr key="transposed-2">
+                        <td key="col-2">Referred By Contact</td>
+                        <td key="data-2-2">
+                          <p key="0">Knight, Bryan J, DO</p>
+                          <p key="1">1 Lincolon St</p>
+                          <p key="2">Ste 2400</p>
+                          <p key="3">Boston, MA 02111</p>
+                          <p key="4">Phone: 508-555-5555</p>
+                          <p key="5">Fax: 781-555-5555</p>
+                          <p key="6">Email: bknight@zushealth.com</p>
+                        </td>
+                      </tr>
+                      <tr key="transposed-3">
+                        <td key="col-3">Referred To Contact</td>
+                        <td key="data-3-3">
+                          <p key="0">Acme Health</p>
+                          <p key="1">200 Jefferson St</p>
+                          <p key="2">Newton, MA 02462-1607</p>
+                          <p key="3">Phone: 617-555-5555</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <table>
+                    <tbody>
+                      <tr key="transposed-0">
+                        <td key="col-0">Referral ID</td>
+                        <td key="data-0-0">789456789456</td>
+                      </tr>
+                      <tr key="transposed-1">
+                        <td key="col-1">Status</td>
+                        <td key="data-1-1">Closed</td>
+                      </tr>
+                      <tr key="transposed-2">
+                        <td key="col-2">Reason</td>
+                        <td key="data-2-2">{null}</td>
+                      </tr>
+                      <tr key="transposed-3">
+                        <td key="col-3">Start Date</td>
+                        <td key="data-3-3">12/25/2022</td>
+                      </tr>
+                      <tr key="transposed-4">
+                        <td key="col-4">Expiration Date</td>
+                        <td key="data-4-4">12/25/2022</td>
+                      </tr>
+                      <tr key="transposed-5">
+                        <td key="col-5">Visits Requested</td>
+                        <td key="data-5-5">1</td>
+                      </tr>
+                      <tr key="transposed-6">
+                        <td key="col-6">Visits Authorized</td>
+                        <td key="data-6-6">1</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <table key="3">
+                    <thead key="1">
+                      <tr key="1">
+                        <th key="1">Scheduling Instructions</th>
+                      </tr>
+                    </thead>
+                    <tbody key="3">
+                      <tr key="1">
+                        <td key="1">
+                          <p key="0">
+                            Office to Office is live in Neurology for the following practice:
+                          </p>
+                          <p key="1">Acme Health Neurology Associates, P.C.</p>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                  <br key="4">{null}</br>
+                  <p key="5">
+                    Electronically signed by Bryan J Knight DO at 12/25/2022 12:39 PM EDT
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        ),
+      },
+    ];
+    test.each(tests)("transpose: $name", ({ value, expected }) => {
+      expect(JSON.stringify(expected)).toStrictEqual(
+        JSON.stringify(recursivelyTransposeTables(value, 0))
+      );
+    });
+  });
+});
--- src/components/content/resource/helpers/details-card.tsx
+++ src/components/content/resource/helpers/details-card.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from "react";
+import parse from "html-react-parser";
+import React, { ReactElement, ReactNode } from "react";
+import { cloneDeep } from "@/utils/nodash";
 
 export type DetailEntry = {
   label?: string;
@@ -11,6 +13,182 @@ export type DetailsProps = {
   documentButton?: ReactNode;
 };
 
+const MIN_SOURCE_COLUMNS_IN_NOTE = 2;
+const MAX_DEPTH_FIND_TABLE_TO_TRANSPOSE = 6;
+
+const WHITESPACE_REGEX = /^\[\\n\\r\\s\\+$/;
+
+export const recursivelyTransposeTables = (node: ReactNode, curDepth: number): ReactNode => {
+  if (!node || curDepth >= MAX_DEPTH_FIND_TABLE_TO_TRANSPOSE) {
+    return node;
+  }
+
+  // if this doesn't have a props then skip it
+  if (node instanceof Boolean) {
+    return node;
+  }
+
+  if (node instanceof String) {
+    if ((node as string).match(WHITESPACE_REGEX)) {
+      return undefined;
+    }
+    return node;
+  }
+
+  // is this node a table?
+  if ((node as ReactElement).type === "table") {
+    const tbl = node as ReactElement;
+    const newTable = transposeTable(tbl);
+    return newTable;
+  }
+
+  const props = Object.hasOwn(node as object, "props")
+    ? (node as { props: Record<string, unknown> }).props
+    : {};
+
+  // if there are contents, then it's a big 'ol string of html we need to parse
+  if (props.content) {
+    // try to render it
+    const el = parse(props.content as string, {});
+    const newChildren = recursivelyTransposeTables(el, curDepth + 1);
+    const retval = cloneDeep(node);
+    (retval as ReactElement).props.content = undefined;
+
+    // react doesn't want arrays of one; rather, it wants either an object or
+    // an array of 2 or more children
+    if (!Array.isArray(newChildren)) {
+      (retval as ReactElement).props.children = newChildren;
+    } else if (newChildren.length === 1 && newChildren[0]) {
+      // eslint-disable-next-line prefer-destructuring
+      (retval as ReactElement).props.children = newChildren[0];
+    } else {
+      (retval as ReactElement).props.children = newChildren;
+    }
+    return retval;
+  }
+
+  if (!props.children) {
+    return node;
+  }
+
+  const children = Array.isArray(props.children)
+    ? (props.children as ReactElement[])
+    : ([props.children] as ReactElement[]);
+
+  const newChildren = (children as ReactElement[]).map((child) =>
+    recursivelyTransposeTables(child, curDepth + 1)
+  );
+  const newNode = cloneDeep(node);
+
+  if (!(newNode as ReactElement).props) {
+    return node;
+  }
+
+  // react doesn't want arrays of one; rather, it wants either an object or
+  // an array of 2 or more children
+  if (newChildren.length === 1 && newChildren[0]) {
+    // eslint-disable-next-line prefer-destructuring
+    (newNode as ReactElement).props.children = newChildren[0];
+  } else {
+    (newNode as ReactElement).props.children = newChildren;
+  }
+  return newNode;
+};
+
+const transposeTable = (tbl: ReactElement): ReactElement => {
+  // find the thead, otherwise skip
+  const theads = (tbl.props.children as ReactElement[]).filter((child) => child.type === "thead");
+  if (theads.length !== 1) {
+    return tbl;
+  }
+  const thead = theads[0];
+
+  // find the tbody, otherwise skip
+  const tbodies = (tbl.props.children as ReactElement[]).filter((child) => child.type === "tbody");
+  if (tbodies.length !== 1) {
+    return tbl;
+  }
+  const tbody = tbodies[0];
+
+  // if there's less than the max number of cols, return the original table
+  let headerRows = [] as ReactElement[];
+  if (Array.isArray(thead.props.children)) {
+    headerRows = thead.props.children as ReactElement[];
+  } else if (
+    thead.props.children.type === "tr" &&
+    Array.isArray(thead.props.children.props.children)
+  ) {
+    headerRows = thead.props.children.props.children as ReactElement[];
+  } else {
+    headerRows = [thead.props.children.props.children] as ReactElement[];
+  }
+
+  if (headerRows.length <= MIN_SOURCE_COLUMNS_IN_NOTE) {
+    return tbl;
+  }
+
+  // header values as the first column in the rows
+  const newRows = [] as JSX.Element[];
+
+  const dataRows = Array.isArray(tbody.props.children)
+    ? (tbody.props.children as ReactElement[])
+    : ([tbody.props.children] as ReactElement[]);
+  if (!dataRows.length) {
+    return tbl;
+  }
+
+  let transposeIdx = 0;
+  dataRows.forEach((dataRow) => {
+    // each data row should equate to N new tranpose rows, one for
+    // each column in the original data table
+    newRows.push(
+      ...headerRows.map((col, colIdx) => (
+        // the child should be just one text value
+        <tr key={`transposed-${transposeIdx}`}>
+          <td
+            key={
+              // eslint-disable-next-line react/no-array-index-key
+              `col-${colIdx}`
+            }
+          >
+            {col.props.children}
+          </td>
+        </tr>
+      ))
+    );
+
+    // TODO: add a classname to denote where to create a stronger line between
+    //       rows
+
+    // now for each cell in the data row
+    const dataCells = Array.isArray(dataRow.props.children)
+      ? (dataRow.props.children as ReactElement[])
+      : ([dataRow.props.children] as ReactElement[]);
+
+    dataCells.forEach((dataCell, cellIdx) => {
+      const tr = newRows[transposeIdx];
+      const currentCells = Array.isArray(tr.props.children)
+        ? (tr.props.children as ReactElement[])
+        : ([tr.props.children] as ReactElement[]);
+      const updatedCells = [
+        ...currentCells,
+        // eslint-disable-next-line react/no-array-index-key
+        <td key={`data-${transposeIdx}-${cellIdx}`}>{dataCell.props.children}</td>,
+      ];
+
+      newRows[transposeIdx] = <tr key={`transposed-${transposeIdx}`}>{updatedCells}</tr>;
+
+      transposeIdx += 1;
+    });
+  });
+
+  return (
+    <table>
+      <tbody>{newRows}</tbody>
+    </table>
+  );
+};
+
 export const DetailsCard = ({ details, hideEmpty = true, documentButton }: DetailsProps) => (
   <div className="ctw-rounded-lg ctw-bg-bg-lighter">
     <dl className="ctw-m-0 ctw-space-y-2 ctw-px-4 ctw-py-6">
@@ -21,18 +199,19 @@ export const DetailsCard = ({ details, hideEmpty = true, documentButton }: Detai
       {details
         .filter((d) => !hideEmpty || d.value || d.value === 0)
         .map(({ label, value }, idx) => {
+          const valueWithTransposedTables = recursivelyTransposeTables(value, 0);
           if (!label) {
             return (
               // eslint-disable-next-line react/no-array-index-key
               <div key={idx} className="ctw-text-gray-900 ctw-flex ctw-items-baseline">
-                <div className="ctw-m-0 ctw-w-full">{value}</div>
+                <div className="ctw-m-0 ctw-w-full">{valueWithTransposedTables}</div>
               </div>
             );
           }
           return (
             <div key={label} className="ctw-text-gray-900 ctw-flex ctw-items-baseline">
               <dt className="ctw-w-1/3 ctw-flex-shrink-0 ctw-font-medium">{label}</dt>
-              <dd className="ctw-m-0 ctw-w-full">{value}</dd>
+              <dd className="ctw-m-0 ctw-w-full">{valueWithTransposedTables}</dd>
             </div>
           );
         })}
