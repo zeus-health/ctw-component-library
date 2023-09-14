5d8b5aa2c95fa002cb6caaed2c82ca46c4fea564
Eric Gunther <97455910+eric-gunther@users.noreply.github.com>
2023-09-14 13:46:48 -0400

    CTW-1393 Search UI improvements (#1203)
    - Modified search to only use keyword search, limit to 10 results, and
    don't search observations. We will continue to tweak this but these
    settings have yielded sensible results so far during testing.
    - Created generic SearchResult component and removed custom components
    for conditions/documents/etc.
    - Modified what data we show for the different search results based on
    feedback during meeting with design. Will continue to iterate on these.
    - Added empty states.
    - Updated styling and made it so the heading is the button to open the
    drawer. Still struggling a bit with getting the text overflow working on
    the medications heading, but may punt on that in the interest of getting
    this out faster for people to validate search results.
    
    ---------
    
    Co-authored-by: Michael Rosata <mrosata@zushealth.com>

---
+++ src/components/content/patient-record-search/helpers/search-result.tsx
@@ -0,0 +1,49 @@
+import fhir4 from "fhir/r4";
+import { FeedbackForm } from "@/components/content/patient-record-search/helpers/feedback-form";
+import { Badge } from "@/components/core/badge";
+import { FHIRModel } from "@/fhir/models/fhir-model";
+
+type SearchResultProps<T extends FHIRModel<fhir4.Resource>> = {
+  resource: T;
+  heading?: string;
+  label: string;
+  openDetails?: (record: T) => void;
+  text?: string | JSX.Element;
+  details?: { label: string; value?: string }[];
+};
+
+export function SearchResult<T extends FHIRModel<fhir4.Resource>>({
+  resource,
+  heading,
+  openDetails,
+  label,
+  text,
+  details,
+}: SearchResultProps<T>) {
+  return (
+    <div className="ctw-patient-record-search-result">
+      <div className="ctw-flex ctw-items-end ctw-justify-between">
+        <span>
+          <button
+            type="button"
+            className="ctw-patient-record-search-result-btn"
+            onClick={() => openDetails?.(resource)}
+          >
+            {heading}
+          </button>
+          <Badge color="primary" text={label} />
+        </span>
+        <FeedbackForm name={`${resource.resourceType}/${resource.id}`} />
+      </div>
+      <div className="ctw-patient-record-search-text">{text}</div>
+
+      <div className="ctw-patient-record-search-details">
+        {details?.map((detail) => (
+          <div key={detail.label} data-label={detail.label}>
+            {detail.value}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
