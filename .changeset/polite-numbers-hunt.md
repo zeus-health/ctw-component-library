e457472f8b10471c4d4658a24b4b3e2706b32112
Eric Gunther <97455910+eric-gunther@users.noreply.github.com>
2023-10-02 17:13:42 -0400

    CTW-1445 First pass on new overview tab (#1260)
    Created a new "Overview" tab that will only be visible if you have
    patient record search enabled. This will serve as our first pass at a
    summary/overview for the patient while also helping to make search more
    prominent for beta customers.
    
    ---------
    
    Co-authored-by: Wesley Reitzfeld <1568246+WesleyKapow@users.noreply.github.com>

---
+++ .changeset/polite-numbers-hunt.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": patch
+---
+
+Add Overview tab for users with access to search.
--- src/components/content/patient-history/patient-history-action.tsx
+++ src/components/content/patient-history/patient-history-action.tsx
@@ -2,12 +2,16 @@ import { PatientHistoryLastRetrieved } from "./patient-history-last-retrieved";
 import { RequestRecordsButton } from "./request-records-button";
 
 export type PatientHistoryActionProps = {
-  hideRequestRecords: boolean;
+  hideRequestRecords?: boolean;
 };
 
-export const PatientHistoryAction = ({ hideRequestRecords = false }: PatientHistoryActionProps) => (
-  <div className="ctw-flex ctw-space-x-1">
-    <PatientHistoryLastRetrieved />
-    {!hideRequestRecords && <RequestRecordsButton />}
-  </div>
+export const PatientHistoryLastRetrievedWithAction = ({
+  hideRequestRecords = false,
+}: PatientHistoryActionProps) => (
+  <>
+    <div>
+      <PatientHistoryLastRetrieved />
+    </div>
+    <div>{!hideRequestRecords && <RequestRecordsButton />}</div>
+  </>
 );
