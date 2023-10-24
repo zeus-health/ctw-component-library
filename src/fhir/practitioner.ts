a321dd00b0d0e9290597ed679d23c71bf7b0109b
Eric Gunther <egunther@zushealth.com>
2023-10-24 10:32:14 -0400

    CTW-1523 Remove practitioner query

--- src/fhir/practitioner.ts
+++ src/fhir/practitioner.ts
@@ -1,52 +1,19 @@
 import { Reference } from "fhir/r4";
-import { searchBuilderRecords } from "./search-helpers";
 import { CTWRequestContext } from "@/components/core/providers/ctw-context";
-import { PractitionerModel } from "@/fhir/models/practitioner";
 import { claimsAuthEmail, claimsPractitionerId } from "@/utils/auth";
-import { withTimerMetric } from "@/utils/telemetry";
 
-const getPractitioner = async (practitionerId: string, requestContext: CTWRequestContext) => {
-  let practitioners = [];
-  try {
-    const { resources } = await searchBuilderRecords("Practitioner", requestContext, {
-      _id: practitionerId,
-    });
-
-    practitioners = resources;
-  } catch (error) {
-    throw new Error(`Failed fetching practitioner with an id of: ${practitionerId}. ${error}`);
-  }
-
-  if (!practitioners[0]) {
-    throw new Error(`No practitioner found with an id of: ${practitionerId}`);
-  }
-
-  return new PractitionerModel(practitioners[0]);
-};
-
-// Returns a reference for the user's practitioner by looking
-// up the practitioner from the user's claims data.
-// If the user does not have an associated practitioner, then we
-// use their email address as the display in the reference.
-async function getUsersPractitionerReferenceReq(
+// Use their email address as the display if no practitioner reference available.
+export async function getUsersPractitionerReference(
   requestContext: CTWRequestContext
 ): Promise<Reference> {
   const practitionerId = claimsPractitionerId(requestContext.authToken);
   if (practitionerId) {
-    const practitioner = await getPractitioner(practitionerId, requestContext);
     return {
       reference: `Practitioner/${practitionerId}`,
       type: "Practitioner",
-      display: practitioner.fullName,
     };
   }
-
   return {
     display: claimsAuthEmail(requestContext.authToken),
   };
 }
-
-export const getUsersPractitionerReference = withTimerMetric(
-  getUsersPractitionerReferenceReq,
-  "req.get_users_practitioner_reference"
-);
---
+++ src/services/fqs/queries/practitioner.ts
@@ -0,0 +1,75 @@
+import { Immunization } from "fhir/r4";
+import { gql } from "graphql-request";
+import { fragmentCoding, fragmentPatient } from "./fragments";
+import { GraphqlConnectionNode, GraphqlPageInfo } from "../client";
+
+export interface ImmunizationConnection {
+  pageInfo: GraphqlPageInfo;
+  edges: GraphqlConnectionNode<Immunization>[];
+}
+
+export interface ImmunizationGraphqlResponse {
+  ImmunizationConnection: ImmunizationConnection;
+}
+
+export const immunizationsQuery = gql`
+  ${fragmentCoding}
+  ${fragmentPatient}
+  query Immunizations($upid: ID!, $cursor: String!, $sort: ImmunizationSortParams!, $first: Int!) {
+    ImmunizationConnection(upid: $upid, after: $cursor, sort: $sort, first: $first) {
+      pageInfo {
+        hasNextPage
+      }
+      edges {
+        node {
+          id
+          resourceType
+          meta {
+            tag {
+              system
+              code
+            }
+            versionId
+          }
+          extension {
+            url
+            valueString
+          }
+          status
+          statusReason {
+            text
+            coding {
+              ...Coding
+            }
+          }
+          vaccineCode {
+            text
+            coding {
+              ...Coding
+            }
+          }
+          patient {
+            reference
+            resource {
+              ... on Patient {
+                ...Patient
+                managingOrganization {
+                  resource {
+                    ... on Organization {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+          occurrenceDateTime
+          occurrenceString
+          recorded
+          lotNumber
+          expirationDate
+        }
+      }
+    }
+  }
+`;
