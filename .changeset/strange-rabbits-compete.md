165a301ff14ba00936b727add45adf5563876654
Michael Rosata <mrosata@zushealth.com>
2023-10-26 09:29:05 -0400

    CTW-1497/Support feature flag ctw-patient-history-form (#1343)
    Support feature flag ctw-patient-history-form. This will hide the
    request records button if this flag is disabled. It is enabled for
    everyone currently.

---
+++ .changeset/strange-rabbits-compete.md
@@ -0,0 +1,5 @@
+---
+"@zus-health/ctw-component-library": minor
+---
+
+Hide request records button when feature flag ctw-patient-history-form is disabled.
--- src/components/content/patient-history/patient-history-action.tsx
+++ src/components/content/patient-history/patient-history-action.tsx
@@ -1,5 +1,6 @@
 import { PatientHistoryLastRetrieved } from "./patient-history-last-retrieved";
 import { RequestRecordsButton } from "./request-records-button";
+import { useFeatureToggle } from "@/hooks/use-feature-toggle";
 
 export type PatientHistoryActionProps = {
   hideRequestRecords?: boolean;
@@ -9,15 +10,19 @@ export type PatientHistoryActionProps = {
 export const PatientHistoryLastRetrievedWithAction = ({
   hideRequestRecords = false,
   includePatientDemographicsForm = false,
-}: PatientHistoryActionProps) => (
-  <>
-    <div>
-      <PatientHistoryLastRetrieved />
-    </div>
-    <div>
-      {!hideRequestRecords && (
-        <RequestRecordsButton includePatientDemographicsForm={includePatientDemographicsForm} />
-      )}
-    </div>
-  </>
-);
+}: PatientHistoryActionProps) => {
+  const hideRequestRecordsFormToggle = !useFeatureToggle("ctw-patient-history-form").enabled;
+
+  return (
+    <>
+      <div>
+        <PatientHistoryLastRetrieved />
+      </div>
+      <div>
+        {!(hideRequestRecords || hideRequestRecordsFormToggle) && (
+          <RequestRecordsButton includePatientDemographicsForm={includePatientDemographicsForm} />
+        )}
+      </div>
+    </>
+  );
+};
