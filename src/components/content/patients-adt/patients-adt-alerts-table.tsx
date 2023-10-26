53ac8febe61fde0dfc4af55a6d025996b61691ae
Melody Liu <mliu@zushealth.com>
2023-10-26 10:58:01 -0400

    Re-organizing

--- src/components/content/patients-adt/patients-adt-alerts-table.tsx
+++ src/components/content/patients-adt/patients-adt-alerts-table.tsx
@@ -4,8 +4,9 @@ import { faFileLines } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import cx from "classnames";
 import { useEffect, useState } from "react";
-import { adtFilter, defaultADTFilters } from "./filters";
-import { useADTAlertDetailsDrawer } from "./modal-hooks";
+import { adtFilter, defaultADTFilters } from "./helpers/filters";
+import { useADTAlertDetailsDrawer } from "./helpers/modal-hooks";
+import { mapEncountersAndNotes, RelatedEncounterMap } from "./helpers/related-encounters";
 import { dedupeAndMergeEncounters } from "../encounters/helpers/filters";
 import { defaultEncounterSort, encounterSortOptions } from "../encounters/helpers/sorts";
 import { TableOptionProps } from "../patients/patients-table";
@@ -16,29 +17,12 @@ import { EmptyTableNoneFound } from "@/components/core/empty-table";
 import { withErrorBoundary } from "@/components/core/error-boundary";
 import { Pagination } from "@/components/core/pagination/pagination";
 import { AnalyticsProvider } from "@/components/core/providers/analytics/analytics-provider";
-import { CTWRequestContext } from "@/components/core/providers/ctw-context";
 import { useCTW } from "@/components/core/providers/use-ctw";
 import { SimpleMoreList } from "@/components/core/simple-more-list";
 import { EncounterModel } from "@/fhir/models/encounter";
 import { useFilteredSortedData } from "@/hooks/use-filtered-sorted-data";
-import { createGraphqlClient, fqsRequest } from "@/services/fqs/client";
-import { EncounterGraphqlResponse, encountersQuery } from "@/services/fqs/queries/encounters";
-import { cloneDeep } from "@/utils/nodash";
-import { QUERY_KEY_ENCOUNTERS_RELATED } from "@/utils/query-keys";
-import { queryClient } from "@/utils/request";
 
 const PAGE_SIZE = 10;
-
-// Cache related encounters and notes for 10 minutes.
-const RELATED_ENC_STALE_TIME = 1000 * 60 * 10;
-
-type RelatedEncounter = {
-  upid: string;
-  cwcqEncounterId: string;
-  binaryId: string;
-};
-
-type RelatedEncounterMap = Map<string, RelatedEncounter>;
 
 export type ADTTableProps = {
   className?: cx.Argument;
@@ -193,76 +177,3 @@ const PatientColumn = ({ patient }: PatientColumnProps) => (
     </div>
   </div>
 );
-
-async function fetchRelatedEncounter(
-  encAndNote: RelatedEncounter,
-  getRequestContext: () => Promise<CTWRequestContext>
-) {
-  const requestContext = await getRequestContext();
-  const graphClient = createGraphqlClient(requestContext);
-  return fqsRequest<EncounterGraphqlResponse>(graphClient, encountersQuery, {
-    upid: encAndNote.upid,
-    cursor: "",
-    first: 1,
-    sort: {
-      lastUpdated: "DESC",
-    },
-    filter: {
-      ids: {
-        anymatch: [encAndNote.cwcqEncounterId],
-      },
-    },
-  });
-}
-
-async function fetchQueryRelatedEncounter(
-  encAndNote: RelatedEncounter,
-  getRequestContext: () => Promise<CTWRequestContext>
-) {
-  return queryClient.fetchQuery(
-    [QUERY_KEY_ENCOUNTERS_RELATED, encAndNote.cwcqEncounterId],
-    async () => fetchRelatedEncounter(encAndNote, getRequestContext),
-    { staleTime: RELATED_ENC_STALE_TIME }
-  );
-}
-
-// Produces a copy of the encounters, but with the related encounter and notes assigned.
-async function mapEncountersAndNotes(
-  adtEncounters: EncounterModel[],
-  encounterAndNotesData: RelatedEncounterMap,
-  getRequestContext: () => Promise<CTWRequestContext>
-): Promise<EncounterModel[]> {
-  const encountersToBeRelated: EncounterModel[] = [];
-  const encountersWithoutRelated: EncounterModel[] = [];
-  const relatedEncounterPromises = [];
-
-  for (const encounter of adtEncounters) {
-    if (encounterAndNotesData.has(encounter.resource.id ?? "")) {
-      const encAndNote = encounterAndNotesData.get(encounter.resource.id ?? "");
-      if (encAndNote) {
-        encountersToBeRelated.push(cloneDeep(encounter));
-        relatedEncounterPromises.push(fetchQueryRelatedEncounter(encAndNote, getRequestContext));
-      }
-    } else {
-      encountersWithoutRelated.push(encounter);
-    }
-  }
-
-  const graphQlResponses = await Promise.all(relatedEncounterPromises);
-  const encounterNodes = graphQlResponses
-    .map((r) => r.data.EncounterConnection.edges.map((x) => x.node))
-    .flat();
-  const encountersRelated = encountersToBeRelated.map((encounterWithRelated, index) => {
-    // Reference enc & note
-    const newEncounter = cloneDeep(encounterWithRelated);
-    const encounterNode = encounterNodes[index];
-    newEncounter.relatedEncounter = new EncounterModel(encounterNode, encounterNode.ProvenanceList);
-    const encAndNote = encounterAndNotesData.get(encounterWithRelated.resource.id ?? "");
-    if (encAndNote) {
-      newEncounter.relatedEncounter.binaryId = encAndNote.binaryId.replaceAll('"', "");
-    }
-    return newEncounter;
-  });
-
-  return [...encountersRelated, ...encountersWithoutRelated];
-}
